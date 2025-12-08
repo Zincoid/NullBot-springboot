26851bbb275caaa41194f8b1226e94e8751e5328
Zincoid <2660181154@qq.com>
2025-12-08 13:21:31 +0800

    Changes

--- src/main/java/org/bot/qqbot/command/reply/HelpCommand.java
+++ src/main/java/org/bot/qqbot/command/reply/HelpCommand.java
@@ -19,23 +19,29 @@ public class HelpCommand implements Command
     public void execute(Bot bot, CommandEvent<?> event) {
         if (event.getEvent() instanceof GroupMessageEvent groupMessageEvent) {
             String help = """
-                    /Chat 或 @Null 或 戳一戳  与AI对话
-                    /ResetHistory  清空历史记忆
-                    /ChatHistory   获取聊天历史
-                    /ChatMode  切换聊天模式
+                    ◉ [AI功能]
+                    Chat 或 @Null 或 戳一戳  与AI对话
+                    ResetHistory  清空历史记忆
+                    ChatHistory   获取聊天历史
+                    ChatMode  切换聊天模式
                     
-                    /ImageSave  保存引用的图片至本地
-                    /ImageGet  获取保存的图片
-                    /ImageDelete  删除保存的图片
-                    /RandomImage  发送保存的随机图片
-                    /ImageList  获取保存图片的列表
+                    ◉ [图片功能]
+                    ImageSave  保存引用的图片至本地
+                    ImageGet  获取保存的图片
+                    ImageDelete  删除保存的图片
+                    ImageList  获取保存图片的列表
+                    RandomImage 或 img  发送保存的随机图片
+
+                    ◉ [管理功能]
+                    UserBan  禁言群内用户
+                    FunctionControl  修改部分功能启用状态
+                    AccessSet  设置用户限权
                     
-                    /UserBan  禁言群内用户
-                    /FunctionControl  修改部分功能启用状态
-                    /AccessSet  设置用户限权
+                    ◉ [帮助功能]
+                    Help  获取帮助信息
+                    指令后加 -help或-h 获取详情
                     
-                    /Help  获取帮助信息
-                    指令后加 -help或-h 获取详情""";
+                    指令前缀为 /""";
 
             bot.sendGroupMsg(groupMessageEvent.getGroupId(), "======= HELP =======\n" + help, false);
             logger.info("\t\t\t\t├─[Help] 已打印帮助");
