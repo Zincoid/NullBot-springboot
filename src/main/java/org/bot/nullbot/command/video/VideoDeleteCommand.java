f434ee8d4b82032d152ae00248e236539ba87927
Zincoid <2660181154@qq.com>
2025-12-10 23:28:07 +0800

    Changes

---
+++ src/main/java/org/bot/nullbot/command/video/VideoDeleteCommand.java
@@ -0,0 +1,70 @@
+package org.bot.nullbot.command.video;
+
+import com.mikuac.shiro.core.Bot;
+import com.mikuac.shiro.dto.action.response.GetMsgResp;
+import com.mikuac.shiro.dto.event.message.GroupMessageEvent;
+import com.mikuac.shiro.enums.MsgTypeEnum;
+import com.mikuac.shiro.model.ArrayMsg;
+import lombok.RequiredArgsConstructor;
+import org.bot.nullbot.annotation.CommandMapping;
+import org.bot.nullbot.command.Command;
+import org.bot.nullbot.config.FileStorageConfig;
+import org.bot.nullbot.entity.CommandEvent;
+import org.bot.nullbot.plugin.util.FileUtil;
+import org.bot.nullbot.plugin.util.MessageParseUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@CommandMapping({"VideoDelete", "删除视频"})
+@Component
+@RequiredArgsConstructor
+public class VideoDeleteCommand implements Command
+{
+    private static final Logger logger = LoggerFactory.getLogger(VideoDeleteCommand.class);
+    private final FileStorageConfig fileStorageConfig;
+
+    @Override
+    public void execute(Bot bot, CommandEvent<?> event) {
+        if (event.getEvent() instanceof GroupMessageEvent groupMessageEvent) {
+            ArrayMsg reply = groupMessageEvent.getArrayMsg().get(0);
+            if(reply.getType() == MsgTypeEnum.reply){
+                GetMsgResp replyMsg = bot.getMsg(Integer.parseInt(reply.getData().get("id"))).getData();
+                Map<String, String> videoMap = MessageParseUtil.parseGroupRawMessageAsVideoMap(replyMsg.getRawMessage());  // 可优化为单个键值对
+                if(!videoMap.isEmpty()) {
+                    for (Map.Entry<String, String> entry : videoMap.entrySet()) {
+                        String fileName = entry.getKey();
+                        String url = entry.getValue();
+                        String response = FileUtil.deleteFileByName(fileStorageConfig.getVideoPath(), fileName);
+                        bot.sendGroupMsg(groupMessageEvent.getGroupId(), "[视频] \uD83D\uDDD1\uFE0F" + response, false);
+                        logger.info("\t\t\t\t├─[Video.Delete] {}", response);
+                    }
+                }else{
+                    bot.sendGroupMsg(groupMessageEvent.getGroupId(), "[视频] ❌未包含可删除视频", false);
+                    logger.info("\t\t\t\t├─[Video.Delete] 未包含可删除视频");
+                }
+            }else if(!event.getCommandParameters().isEmpty()){
+                String fileName = event.getCommandParameters().get(0);
+                    String response = FileUtil.deleteFileByName(fileStorageConfig.getVideoPath(), fileName);
+                bot.sendGroupMsg(groupMessageEvent.getGroupId(), "[视频] \uD83D\uDDD1\uFE0F" + response, false);
+                logger.info("\t\t\t\t├─[Video.Delete] {}", response);
+            }else{
+                bot.sendGroupMsg(groupMessageEvent.getGroupId(), "[视频] ❌无删除参数或引用", false);
+                logger.info("\t\t\t\t├─[Video.Delete] 无删除参数或引用");
+            }
+        }else
+            logger.info("\t\t\t\t├─[Video.Delete] 未设计 - 非群消息事件响应方式");
+    }
+
+    @Override
+    public Integer getAccess() {
+        return 2;
+    }
+
+    @Override
+    public String getHelp() {
+        return "◉ VideoDelete 命令\n功能: 删除保存的视频\n限权: " + getAccess() + "\n格式: VideoDelete [文件名] 或 [引用视频]VideoDelete\n中文命令: 删除视频";
+    }
+}
