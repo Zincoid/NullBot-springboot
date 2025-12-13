42551a5dd7ee5aabe5c78ef7566b70b07099be88
Zincoid <2660181154@qq.com>
2025-12-14 00:41:33 +0800

    Changes

--- src/main/java/org/bot/nullbot/command/game/basic/DisMatchCommand.java
+++ src/main/java/org/bot/nullbot/command/game/basic/DisMatchCommand.java
@@ -6,7 +6,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.bot.nullbot.annotation.CommandMapping;
 import org.bot.nullbot.command.Command;
-import org.bot.nullbot.component.game.MatchManager;
 import org.bot.nullbot.component.game.Matcher;
 import org.bot.nullbot.entity.CommandEvent;
 import org.bot.nullbot.entity.game.basic.MatchResult;
