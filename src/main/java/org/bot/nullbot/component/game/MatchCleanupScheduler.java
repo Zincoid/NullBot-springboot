f24807c56a696d6ca1e0b62a8c48b79a89a181b0
Zincoid <2660181154@qq.com>
2025-12-14 01:09:40 +0800

    Changes

--- src/main/java/org/bot/nullbot/component/game/MatchCleanupScheduler.java
+++ src/main/java/org/bot/nullbot/component/game/MatchCleanupScheduler.java
@@ -1,20 +1,29 @@
 package org.bot.nullbot.component.game;
 
+import com.mikuac.shiro.core.Bot;
+import com.mikuac.shiro.core.BotContainer;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.bot.nullbot.config.MatchConfig;
 import org.bot.nullbot.entity.game.basic.Match;
+import org.bot.nullbot.entity.game.basic.Player;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Component
 @Slf4j
 @RequiredArgsConstructor
 public class MatchCleanupScheduler
 {
+    @Value("${nullbot.self-id}")
+    private Long selfId;
+    private final BotContainer botContainer;
+
     private final MatchPoolManager poolManager;
     private final MatchManager matchManager;
     private final PlayerManager playerManager;
@@ -35,17 +44,14 @@ public class MatchCleanupScheduler
      * 清理等待匹配超时的玩家
      */
     private void cleanWaitingPlayers() {
+        Bot bot = botContainer.robots.get(selfId);
         LocalDateTime now = LocalDateTime.now();
         poolManager.getAllPools().forEach((gameType, queue) -> queue.removeIf(p -> {
-            // if (p.getStatus() != Player.PlayerStatus.WAITING) { return true; }
-            // if (p.getLastActionTime() == null) { return false; }  // 冗余
             long seconds = Duration.between(p.getLastActionTime(), now).getSeconds();
             if (seconds >= matchConfig.getWaitingTimeoutSeconds()) {
                 log.info("清理匹配超时玩家: {}", p.getUserId());
+                bot.sendGroupMsg(p.getGroupId(), p.getUserName() + "(" + p.getUserId() + ") 匹配超时", false);
                 playerManager.resetPlayer(p);
-
-                // TODO：通知群聊 & 玩家
-
                 return true;
             }
             return false;
@@ -56,6 +62,7 @@ public class MatchCleanupScheduler
      * 清理对局超时（无操作）
      */
     private void cleanTimeoutMatches() {
+        Bot bot = botContainer.robots.get(selfId);
         LocalDateTime now = LocalDateTime.now();
         matchManager.getAllMatches().forEach(match -> {
             if (match.getStatus() != Match.MatchStatus.PLAYING) { return; }
@@ -64,10 +71,13 @@ public class MatchCleanupScheduler
             long seconds = Duration.between(lastActionTime, now).getSeconds();
             if (seconds >= matchConfig.getPlayingTimeoutSeconds()) {
                 log.warn("Match {} 超时未响应自动结束", match.getMatchId());
+                Player p1 = match.getPlayer1();
+                Player p2 = match.getPlayer2();
+                if(!Objects.equals(p1.getGroupId(), p2.getGroupId())){
+                    bot.sendGroupMsg(p1.getGroupId(), p1.getUserName() + "(" + p1.getUserId() + ")\n" + p2.getUserName() + "(" + p2.getUserId() + ")\n对局已超时", false);
+                }
+                bot.sendGroupMsg(p2.getGroupId(), p1.getUserName() + "(" + p1.getUserId() + ")\n" + p2.getUserName() + "(" + p2.getUserId() + ")\n对局已超时", false);
                 matcher.finishMatch(match.getMatchId());
-
-                // TODO：通知群聊 & 玩家
-
             }
         });
     }
