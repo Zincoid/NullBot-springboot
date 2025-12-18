d4fa769207313c10d0a58ae640828772c70a3bd0
Zincoid <2660181154@qq.com>
2025-12-19 00:29:18 +0800

    Changes

--- src/main/java/org/bot/nullbot/command/image/ImageFolderStructureCommand.java
+++ src/main/java/org/bot/nullbot/command/image/ImageFolderStructureCommand.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
-@CommandMapping({"ImageFolderStructure", "图片目录结构"})
+@CommandMapping({"ImageFolder", "图片目录"})
 @Component
 @RequiredArgsConstructor
 @Slf4j
@@ -25,14 +25,14 @@ public class ImageFolderStructureCommand implements Command
     public void execute(Bot bot, CommandEvent<?> event) throws IOException {
         if (event.getEvent() instanceof GroupMessageEvent groupMessageEvent) {
             String structure = FileUtil.getFolderTreeString(fileStorageConfig.getImagePath(), 0);
-            bot.sendGroupMsg(groupMessageEvent.getGroupId(), "[图片目录结构] \uD83D\uDCC1已获取！\n" + structure, false);
-            log.info("\t\t\t\t├─[Image.FolderStructure] 已输出 - 图片目录结构");
+            bot.sendGroupMsg(groupMessageEvent.getGroupId(), "[图片目录] \uD83D\uDCC1已获取！\n" + structure, false);
+            log.info("\t\t\t\t├─[Image.Folder] 已输出 - 图片目录结构");
         } else
-            log.info("\t\t\t\t├─[Image.FolderStructure] 未设计 - 非群消息事件响应方式");
+            log.info("\t\t\t\t├─[Image.Folder] 未设计 - 非群消息事件响应方式");
     }
 
     @Override
     public String getHelp() {
-        return "◉ ImageFolderStructure 命令\n功能: 获取图片目录树结构\n限权: " + getAccess() + "\n格式: ImageFolderStructure\n中文命令: 图片目录结构";
+        return "◉ ImageFolder 命令\n功能: 获取图片目录树结构\n限权: " + getAccess() + "\n格式: ImageFolder\n中文命令: 图片目录";
     }
 }
