7df2056fd12d8cecf37e59e5ec8faec783d07db5
Zincoid <2660181154@qq.com>
2026-01-03 20:31:23 +0800

    NullBot 后端SpringBoot工程

--- src/main/java/org/bot/nullbot/command/ai/embedding/MemeCommand.java
+++ src/main/java/org/bot/nullbot/command/ai/embedding/MemeCommand.java
@@ -50,7 +50,7 @@ public class MemeCommand implements Command
             bot.sendGroupMsg(groupId, response, false);
             log.info("\t\t\t\t├─[Meme] 已发送表情: {}", memeName);
         }else{
-            bot.sendGroupMsg(groupId, "[表情] ❌" + memeName + "不存在", false);
+            bot.sendGroupMsg(groupId, "[表情] ❌" + memeName + " 不存在", false);
             log.info("\t\t\t\t├─[Meme] 表情不存在");
         }
     }
@@ -67,11 +67,11 @@ public class MemeCommand implements Command
                 功能: 发送表情包图片
                 限权: %d
                 格式: 65275d24-ada6-49de-8a40-01c05ad279ea [表情文件名]
-                可使用表情名(名称指代为 图中主体人物_表达的文字内容或情绪.文件扩展名)：
+                可使用表情文件列表(文件名指代为 图中主体人物_表达的文字内容或情绪.文件扩展名)：
                 %s
-                示例: 65275d24-ada6-49de-8a40-01c05ad279ea 猫娘_卖萌.jpg
-                注意: 你可以根据自己的想法发送表情包图片以表达自己的情绪，要经常发表情！
-                (重要！)你只能用提供给你的完整表情名！不要用下划线把不同文件名的主体人物和表达内容情绪的文本拼接起来使用，这种文件不存在！""",
+                示例: 65275d24-ada6-49de-8a40-01c05ad279ea 女孩_干嘛.jpg
+                注意: 你可以发送表情包图片以表达自己的情绪，要经常发表情！
+                (重要！)你只能用提供给你的完整表情文件名！不要用下划线把不同文件名的主体人物和表达内容情绪的文本拼接起来使用，这种文件不存在！""",
                 getAccess(),
                 FileUtil.getFileListAsString(deepSeekConfig.getMemePath())
         );
