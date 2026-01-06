bd418fcc01a0cfec73af6a9c418cfe770a88654d
Zincoid <2660181154@qq.com>
2026-01-06 13:49:08 +0800

    NullBot 后端SpringBoot工程

--- src/main/java/org/bot/nullbot/command/game/basic/SellCommand.java
+++ src/main/java/org/bot/nullbot/command/game/basic/SellCommand.java
@@ -7,12 +7,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.bot.nullbot.annotation.CommandMapping;
 import org.bot.nullbot.command.Command;
 import org.bot.nullbot.entity.CommandEvent;
-import org.bot.nullbot.entity.po.ItemPO;
 import org.bot.nullbot.entity.po.UserPO;
+import org.bot.nullbot.enums.Rarity;
 import org.bot.nullbot.service.InventoryService;
-import org.bot.nullbot.service.ItemService;
 import org.bot.nullbot.service.UserService;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @CommandMapping({"Sell", "出售"})
 @Component
@@ -21,24 +22,44 @@ import org.springframework.stereotype.Component;
 public class SellCommand implements Command
 {
     private final InventoryService inventoryService;
-    private final ItemService itemService;
     private final UserService userService;
 
     @Override
     public void execute(Bot bot, CommandEvent<?> event) throws Exception {
         if (event.getEvent() instanceof GroupMessageEvent groupMessageEvent) {
-            if(event.getCommandParameters().isEmpty()){
+            List<String> params = event.getCommandParameters();
+
+            if(params.isEmpty()){
                 bot.sendGroupMsg(groupMessageEvent.getGroupId(), "[出售] ❌参数不足", false);
                 log.info("\t\t\t\t├─[Sell] 参数不足");
+                return;
+            }
+
+            if("-r".equals(params.get(0))){
+                if(params.size() >= 2){
+                    Rarity rarity = null;
+                    try {
+                        rarity = Rarity.valueOf(params.get(1));
+
+                        bot.sendGroupMsg(groupMessageEvent.getGroupId(), "[出售] ✅已出售" + rarity.getDescription() + "色物品！", false);
+                        log.info("\t\t\t\t├─[Sell] 按稀有度出售成功 - {}", rarity);
+                    } catch (IllegalArgumentException e) {
+                        bot.sendGroupMsg(groupMessageEvent.getGroupId(), "[出售] ❌稀有度参数错误", false);
+                        log.info("\t\t\t\t├─[Sell] 稀有度参数错误");
+                    }
+                }else{
+                    bot.sendGroupMsg(groupMessageEvent.getGroupId(), "[出售] ❌未指定稀有度", false);
+                    log.info("\t\t\t\t├─[Sell] 未指定稀有度");
+                }
                 return;
             }
 
             int itemId;
             int amount = 1;
             try {
-                itemId = Integer.parseInt(event.getCommandParameters().get(0));
-                if(event.getCommandParameters().size() >= 2){
-                    amount = Integer.parseInt(event.getCommandParameters().get(1));
+                itemId = Integer.parseInt(params.get(0));
+                if(params.size() >= 2){
+                    amount = Integer.parseInt(params.get(1));
                     if(amount <= 0){
                         bot.sendGroupMsg(groupMessageEvent.getGroupId(), "[出售] ❌数量非正", false);
                         log.info("\t\t\t\t├─[Sell] 数量非正");
@@ -68,9 +89,11 @@ public class SellCommand implements Command
     public String getHelp() {
         return String.format("""
                 ◉ Sell 命令
-                功能: 出售库存物品
+                功能: 出售库存物品(可批量出售所有指定稀有度物品)
                 限权: %d
                 格式: Sell [物品ID] [可选: 数量(默认为1)]
+                或 Sell -r [稀有度]
+                稀有度: RED/GOLD/PURPLE/BLUE/GREEN/WHITE
                 中文命令: 出售""", getAccess()
         );
     }
