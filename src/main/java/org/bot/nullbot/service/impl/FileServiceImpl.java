39124b1db926ab6a500737c52379f6fa7efd1f9e
Zincoid <2660181154@qq.com>
2026-01-11 18:20:02 +0800

    NullBot 后端SpringBoot工程

--- src/main/java/org/bot/nullbot/service/impl/FileServiceImpl.java
+++ src/main/java/org/bot/nullbot/service/impl/FileServiceImpl.java
@@ -127,7 +127,7 @@ public class FileServiceImpl implements FileService
     }
 
     @Override
-    @Transactional
+    @Transactional(isolation = Isolation.SERIALIZABLE)
     public void syncFilesToDatabase() {
         scanAndSyncFiles();
     }
