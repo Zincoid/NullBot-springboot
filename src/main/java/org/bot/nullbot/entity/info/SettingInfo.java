834c7688aba55cff2b836e8e81e39199fad4379f
Zincoid <2660181154@qq.com>
2026-01-12 02:00:28 +0800

    NullBot 后端SpringBoot工程

--- src/main/java/org/bot/nullbot/entity/info/SettingInfo.java
+++ src/main/java/org/bot/nullbot/entity/info/SettingInfo.java
@@ -39,16 +39,15 @@ public class SettingInfo
     @Override
     public String toString() {
         return String.format("""
-                [Monitor 设置]
+                ◉ Monitor 设置
                 图片收集 - %s
                 消息收集 - %s
                 关键词检测 - %s
                 戳一戳检测 - %s
                 防撤回 - %s
-                [Guess 设置]
+                ◉ Guess 设置
                 切割比例 - %s
-                内边距 - %s
-                """,
+                内边距 - %s""",
 
                 imageCollect ? "ON" : "OFF",
                 messageCollect ? "ON" : "OFF",
