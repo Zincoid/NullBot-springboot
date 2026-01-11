1806d85ffad3d88ac37b829931315dea9177cbe7
Zincoid <2660181154@qq.com>
2026-01-12 03:29:25 +0800

    NullBot 后端SpringBoot工程

---
+++ src/main/java/org/bot/nullbot/entity/ChatOption.java
@@ -0,0 +1,16 @@
+package org.bot.nullbot.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.bot.nullbot.enums.Scope;
+
+@Data
+@AllArgsConstructor
+public class ChatOption
+{
+    private Scope scope;
+    private boolean antiInjection;
+    private boolean thinking;
+    private boolean embedding;
+    private boolean embeddingAuth;
+}
