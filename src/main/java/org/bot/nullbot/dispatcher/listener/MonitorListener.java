199688b51659a091ef00292ff227b01bbed0a9f3
Zincoid <2660181154@qq.com>
2026-01-13 17:38:25 +0800

    NullBot 后端SpringBoot工程

--- src/main/java/org/bot/nullbot/dispatcher/listener/MonitorListener.java
+++ src/main/java/org/bot/nullbot/dispatcher/listener/MonitorListener.java
@@ -45,7 +45,7 @@ public class MonitorListener
     // =================== 串行监听方法 ===================
 
     @FunctionControl(config = "ImgCollect")
-    public void onGroupImageCollection(Bot bot, GroupMessageEvent event) {
+    public void onGroupImageCollection(Bot bot, GroupMessageEvent event) {  // 群目录不存在时数据库无法插入详情文件条目 需手动SYNC
         if(!settingManager.isImageCollect(event.getGroupId())) return;
         boolean hasLogged = false;
         for(ArrayMsg msg : event.getArrayMsg()){
