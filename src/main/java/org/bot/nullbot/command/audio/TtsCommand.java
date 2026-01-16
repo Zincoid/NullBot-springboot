f5a3ef863e671ce0a398648825588ccad8bb9be5
Zincoid <2660181154@qq.com>
2026-01-16 18:33:30 +0800

    NullBot 后端SpringBoot工程

--- src/main/java/org/bot/nullbot/command/audio/TtsCommand.java
+++ src/main/java/org/bot/nullbot/command/audio/TtsCommand.java
@@ -3,6 +3,8 @@ package org.bot.nullbot.command.audio;
 import com.mikuac.shiro.common.utils.MsgUtils;
 import com.mikuac.shiro.core.Bot;
 import com.mikuac.shiro.dto.event.message.GroupMessageEvent;
+import com.mikuac.shiro.enums.MsgTypeEnum;
+import com.mikuac.shiro.model.ArrayMsg;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.bot.nullbot.annotation.CommandMapping;
@@ -11,6 +13,7 @@ import org.bot.nullbot.component.ai.TtsClient;
 import org.bot.nullbot.entity.CommandEvent;
 import org.bot.nullbot.exception.NullBotLogException;
 import org.bot.nullbot.exception.NullBotMsgException;
+import org.bot.nullbot.service.TtsTemplateService;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -21,25 +24,42 @@ import java.util.List;
 @RequiredArgsConstructor
 public class TtsCommand implements Command
 {
+    private final TtsTemplateService ttsTemplateService;
     private final TtsClient ttsClient;
 
     @Override
     public void execute(Bot bot, CommandEvent<?> event) {
         if (event.getEvent() instanceof GroupMessageEvent groupMessageEvent) {
             List<String> params = event.getCommandParameters();
-            if (params.isEmpty()) throw new NullBotMsgException("[语音合成] ❌无参数");
-            String message = String.join(" ", params.subList(0, params.size()));
-            String base64;
-            try {
-                base64 = ttsClient.synthesize(message);
-            } catch (Exception e) {
-                throw new NullBotMsgException("[语音合成] ❌" + e.getMessage());
+            if (params.size() < 2) throw new NullBotMsgException("[语音合成] ❌参数不足");
+
+            String option = params.getFirst();
+            if ("-clone".equals(option)) {
+                switch (params.get(1)) {
+                    case "save" -> {
+                        ArrayMsg reply = groupMessageEvent.getArrayMsg().getFirst();
+                        if (reply.getType() != MsgTypeEnum.reply)
+                            throw new NullBotMsgException("[语音合成] ❌需引用模板音频");
+                        if (params.size() < 4)
+                            throw new NullBotMsgException("[语音合成] ❌新模板参数不足");
+                        // ttsTemplateService.addTemplate();
+                    }
+                }
             }
-            String response = MsgUtils.builder()
-                    .voice("base64://" + base64)
-                    .build();
-            bot.sendGroupMsg(groupMessageEvent.getGroupId(), response, false);
-            log.info("\t\t\t\t├─[Tts] 已回复语音: {}", message.replaceAll("\\R", " "));
+            if ("-synth".equals(option)) {
+                String message = String.join(" ", params.subList(1, params.size()));
+                String base64;
+                try {
+                    base64 = ttsClient.synthesize(message);
+                } catch (Exception e) {
+                    throw new NullBotMsgException("[语音合成] ❌" + e.getMessage());
+                }
+                String response = MsgUtils.builder()
+                        .voice("base64://" + base64)
+                        .build();
+                bot.sendGroupMsg(groupMessageEvent.getGroupId(), response, false);
+                log.info("\t\t\t\t├─[Tts] 已回复合成语音: {}", message.replaceAll("\\R", " "));
+            }
         }else
             throw new NullBotLogException("[语音合成] ❌未设计 - 非群消息事件响应方式");
     }
