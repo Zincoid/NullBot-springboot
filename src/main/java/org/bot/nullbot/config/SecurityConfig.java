ad8644567977a6d5b82a9662c770610cb3f7f02b
Zincoid <2660181154@qq.com>
2026-01-17 23:08:27 +0800

    NullBot 后端SpringBoot工程

--- src/main/java/org/bot/nullbot/config/SecurityConfig.java
+++ src/main/java/org/bot/nullbot/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.bot.nullbot.config.prop.JwtProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.encrypt.KeyStoreKeyFactory;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -17,9 +18,15 @@ public class SecurityConfig
     // Spring Security 拦截
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(auth ->
-                        auth.anyRequest().permitAll()  // 允许所有请求
-                );
+        http.authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/**").permitAll()
+                        .anyRequest().permitAll()  // 允许所有请求
+        )
+                .csrf(AbstractHttpConfigurer::disable)  // 禁用 CSRF
+                .formLogin(AbstractHttpConfigurer::disable)  // 禁用表单登录
+                .httpBasic(AbstractHttpConfigurer::disable)  // 禁用 HTTP Basic
+                .logout(AbstractHttpConfigurer::disable);  // 禁用默认登出
+
         return http.build();
     }
 
