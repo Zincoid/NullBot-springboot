c38c571ea43905d3d7ca28a8c05755bbbcba7243
Zincoid <2660181154@qq.com>
2026-01-20 18:59:19 +0800

    NullBot 后端SpringBoot工程

---
+++ src/main/java/org/bot/nullbot/command/convert/ChoyenCommand.java
@@ -0,0 +1,81 @@
+package org.bot.nullbot.command.convert;
+
+import com.mikuac.shiro.common.utils.MsgUtils;
+import com.mikuac.shiro.core.Bot;
+import com.mikuac.shiro.dto.event.message.GroupMessageEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.bot.nullbot.annotation.CommandMapping;
+import org.bot.nullbot.command.Command;
+import org.bot.nullbot.component.render.HtmlRenderer;
+import org.bot.nullbot.component.resource.ResourceLoader;
+import org.bot.nullbot.config.prop.FileStorageProperties;
+import org.bot.nullbot.entity.CommandEvent;
+import org.bot.nullbot.exception.NullBotLogException;
+import org.bot.nullbot.exception.NullBotMsgException;
+import org.bot.nullbot.util.HtmlTemplateUtil;
+import org.springframework.stereotype.Component;
+
+import java.nio.file.Path;
+import java.util.*;
+
+@CommandMapping({"Choyen", "5000兆"})
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class ChoyenCommand implements Command
+{
+    private final FileStorageProperties fileStorageProperties;
+
+    private final ResourceLoader resourceLoader;
+    private final HtmlRenderer htmlRenderer;
+
+    @Override
+    public void execute(Bot bot, CommandEvent<?> event) {
+        if (event.getEvent() instanceof GroupMessageEvent groupMessageEvent) {
+            Long groupId = groupMessageEvent.getGroupId();
+            List<String> params = event.getCommandParameters();
+            if (params.size() < 2) throw new NullBotMsgException("[5000兆] ❌需要两个参数");
+
+            String base64;
+            String tempFilePath = fileStorageProperties.getTempPath();
+            try {
+                Path htmlPath = resourceLoader.getCached("static/html/5000choyen.html", tempFilePath + "/html");
+                Map<String, String> variables = new HashMap<>();
+                variables.put("textTop", params.get(0));
+                variables.put("textBottom", params.get(1));
+                String html = HtmlTemplateUtil.loadTemplate(htmlPath.toString());
+                html = HtmlTemplateUtil.replaceVariables(html, variables);
+                base64 = htmlRenderer.renderElement(html, "#templateContainer");
+            } catch (Exception e) {
+                throw new NullBotMsgException("[5000兆] ❌处理时出错: " + e.getMessage());
+            }
+            String response = MsgUtils.builder().img("base64://" + base64).build();
+            bot.sendGroupMsg(groupId, response, false);
+            log.info("\t\t\t\t├─[Choyen] 处理完成 - {} {}", params.get(0), params.get(1));
+        }else
+            throw new NullBotLogException("[5000兆] ❌未设计 - 非群消息事件响应方式");
+    }
+
+    @Override
+    public String getHelp() {
+        return String.format("""
+                ◉ Choyen 命令
+                功能: 5000兆円梗图生成
+                限权: %d 级
+                格式: Choyen [文本1] [文本2]
+                别名: 5000兆""", getAccess()
+        );
+    }
+
+    @Override
+    public String getHelpForAI() {
+        return String.format("""
+                ◉ Choyen 命令
+                功能: 5000兆円梗图生成
+                限权: %d 级
+                格式: Choyen [文本1] [文本2]
+                示例: Choyen 我去 不早说""", getAccess()
+        );
+    }
+}
