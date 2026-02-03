1c0a350627f8d31c71a67c72e82dfb3ae53156ed
Zincoid <2660181154@qq.com>
2026-02-03 10:31:34 +0800

    NullBot 后端SpringBoot工程

--- src/main/java/org/bot/nullbot/QqBotApplication.java
+++ src/main/java/org/bot/nullbot/QqBotApplication.java
@@ -2,16 +2,29 @@ package org.bot.nullbot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableAsync
 @EnableScheduling
-public class QqBotApplication {
+public class QqBotApplication
+{
+    private static ConfigurableApplicationContext context;
+    private static String[] args;
 
     public static void main(String[] args) {
-        SpringApplication.run(QqBotApplication.class, args);
+        QqBotApplication.args = args;
+        QqBotApplication.context = SpringApplication.run(QqBotApplication.class, args);
     }
 
+    public static void restart() {
+        Thread thread = new Thread(() -> {
+            context.close();
+            context = SpringApplication.run(QqBotApplication.class, args);
+        });
+        thread.setDaemon(false);
+        thread.start();
+    }
 }
