7186dd90996f1b000c4e924cf65827a31f057995
Zincoid <2660181154@qq.com>
2026-02-09 11:53:28 +0800

    NullBot 后端SpringBoot工程

--- src/main/java/org/bot/nullbot/dispatcher/handler/impl/PermissionHandler.java
+++ src/main/java/org/bot/nullbot/dispatcher/handler/impl/PermissionHandler.java
@@ -26,61 +26,50 @@ public class PermissionHandler implements Handler
 
     @Override
     public void handle(Bot bot, Command command, CommandEvent<?> event, CommandHandlerChain chain) throws Exception {
-        int commandAccess = command.getAccess();
+        Long groupId;
+        Long userId;
 
         if (event.getEvent() instanceof GroupMessageEvent groupMessageEvent) {
-            int groupAccess = groupService.getGroupAccess(groupMessageEvent.getGroupId());
-            if(groupAccess >= commandAccess){
-                log.info("\t\t├─[PermissionHandler] 群限权满足");
-            }else{
-                log.info("\t\t├─[PermissionHandler] 群限权不足");
-                return;
-            }
-            if(event.isAuthRequired()){
-                int userAccess = userService.getUserAccess(groupMessageEvent.getSender().getUserId());
-                if (userAccess >= commandAccess) {
-                    log.info("\t\t├─[PermissionHandler] 用户限权满足");
-                    chain.doHandle(bot, event, command);
-                }else{
-                    log.info("\t\t├─[PermissionHandler] 用户限权不足");
-                    bot.sendGroupMsg(groupMessageEvent.getGroupId(), "[Access] \uD83D\uDEAB限权不足\n需要限权等级: " + commandAccess + "\n你的限权等级: " + userAccess, false);
-                }
-            }else{
-                log.info("\t\t├─[PermissionHandler] 无需验证用户限权");
-                chain.doHandle(bot, event, command);
-            }
+            groupId = groupMessageEvent.getGroupId();
+            userId = groupMessageEvent.getSender().getUserId();
         } else if (event.getEvent() instanceof PokeNoticeEvent pokeNoticeEvent) {
-            int groupAccess = groupService.getGroupAccess(pokeNoticeEvent.getGroupId());
-            if(groupAccess >= commandAccess){
-                log.info("\t\t├─[PermissionHandler] 群限权满足");
-            }else{
-                log.info("\t\t├─[PermissionHandler] 群限权不足");
-                return;
-            }
-            if(event.isAuthRequired()){
-                int userAccess = userService.getUserAccess(pokeNoticeEvent.getUserId());
-                if (userAccess >= commandAccess) {
-                    log.info("\t\t├─[PermissionHandler] 用户限权满足");
-                    chain.doHandle(bot, event, command);
-                }else{
-                    log.info("\t\t├─[PermissionHandler] 用户限权不足");
-                    bot.sendGroupMsg(pokeNoticeEvent.getGroupId(), "[Access] \uD83D\uDEAB限权不足\n需要限权等级: " + commandAccess + "\n你的限权等级: " + userAccess, false);
-                }
-            }else{
-                log.info("\t\t├─[PermissionHandler] 无需验证用户限权");
-                chain.doHandle(bot, event, command);
-            }
+            groupId = pokeNoticeEvent.getGroupId();
+            userId = pokeNoticeEvent.getUserId();
         } else if (event.getEvent() instanceof GroupMsgDeleteNoticeEvent  groupMsgDeleteNoticeEvent) {
-            int groupAccess = groupService.getGroupAccess(groupMsgDeleteNoticeEvent.getGroupId());
-            if(groupAccess >= commandAccess){
-                log.info("\t\t├─[PermissionHandler] 群限权满足");
-                chain.doHandle(bot, event, command);
-            }else{
-                log.info("\t\t├─[PermissionHandler] 群限权不足");
-            }
+            groupId = groupMsgDeleteNoticeEvent.getGroupId();
+            userId = groupMsgDeleteNoticeEvent.getUserId();
         } else {
             log.info("\t\t├─[PermissionHandler] 默认通过的事件类型");
             chain.doHandle(bot, event, command);
+            return;
         }
+
+        int commandAccess = command.getAccess();
+        int groupAccess = groupService.getGroupAccess(groupId);
+        int userAccess = userService.getUserAccess(userId);
+
+        if (groupAccess >= commandAccess) {
+            log.info("\t\t├─[PermissionHandler] 群限权满足");
+        } else {
+            log.info("\t\t├─[PermissionHandler] 群限权不足");
+            return;
+        }
+
+        if (event.isAuthRequired()) {
+            if (userAccess >= commandAccess) {
+                log.info("\t\t├─[PermissionHandler] 用户限权满足");
+            } else {
+                log.info("\t\t├─[PermissionHandler] 用户限权不足");
+                bot.sendGroupMsg(groupId, """
+                        [Access] \uD83D\uDEAB限权不足
+                        需要限权等级: %s
+                        你的限权等级: %s""".formatted(commandAccess, userAccess), false);
+                return;
+            }
+        } else {
+            log.info("\t\t├─[PermissionHandler] 无需验证用户限权");
+        }
+
+        chain.doHandle(bot, event, command);
     }
 }
