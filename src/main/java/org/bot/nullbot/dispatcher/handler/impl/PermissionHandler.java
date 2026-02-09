c2130db3a2e4ea344a67fe2adf7b93c5919a8f06
Zincoid <2660181154@qq.com>
2026-02-09 13:09:50 +0800

    NullBot 后端SpringBoot工程

--- src/main/java/org/bot/nullbot/dispatcher/handler/impl/PermissionHandler.java
+++ src/main/java/org/bot/nullbot/dispatcher/handler/impl/PermissionHandler.java
@@ -15,17 +15,26 @@ import org.bot.nullbot.service.UserService;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 @Order(0)
 @Component
 @RequiredArgsConstructor
 @Slf4j
 public class PermissionHandler implements Handler
 {
+    private final Map<Long, List<String>> banMap = new ConcurrentHashMap<>();  // GroupId -> Commands
+
     private final GroupService groupService;
     private final UserService userService;
 
     @Override
     public void handle(Bot bot, Command command, CommandEvent<?> event, CommandHandlerChain chain) throws Exception {
+        String commandType = event.getCommandType();
+        List<String> params = event.getCommandParameters();
         Long groupId;
         Long userId;
 
@@ -62,14 +71,46 @@ public class PermissionHandler implements Handler
                 log.info("\t\t├─[PermissionHandler] 用户限权不足");
                 bot.sendGroupMsg(groupId, """
                         [Access] \uD83D\uDEAB限权不足
-                        需要限权等级: %s
-                        你的限权等级: %s""".formatted(commandAccess, userAccess), false);
+                        - 需要限权等级: %s
+                        - 你的限权等级: %s""".formatted(commandAccess, userAccess), false);
                 return;
             }
         } else {
             log.info("\t\t├─[PermissionHandler] 无需验证用户限权");
         }
 
+        if (!params.isEmpty() && "-x".equals(params.getFirst())) {
+            onBanningRefresh(bot, userAccess, groupId, commandType);
+            return;
+        }
+
+        if (banMap.computeIfAbsent(groupId, k -> new ArrayList<>()).contains(commandType)) {
+            log.info("\t\t├─[PermissionHandler] 群组 {} - {} 停用中", groupId, commandType);
+            bot.sendGroupMsg(groupId, "[Access] ⛔️停用中", false);
+            return;
+        }
+
         chain.doHandle(bot, event, command);
+    }
+
+    private void onBanningRefresh(Bot bot, int userAccess, Long groupId, String commandType) {
+        if (userAccess < 1) {
+            log.info("\t\t├─[PermissionHandler] 修改限权不足");
+            bot.sendGroupMsg(groupId, """
+                    [Access] \uD83D\uDEAB限权不足
+                    - 需要限权等级: 1
+                    - 你的限权等级: %s""".formatted(userAccess), false);
+            return;
+        }
+        List<String> banned = banMap.computeIfAbsent(groupId, k -> new ArrayList<>());
+        if (banned.contains(commandType)) {
+            banned.remove(commandType);
+            log.info("\t\t├─[PermissionHandler] 群组 {} - {} 已恢复", groupId, commandType);
+            bot.sendGroupMsg(groupId, "[Access] ✅已恢复", false);
+        } else {
+            banned.add(commandType);
+            log.info("\t\t├─[PermissionHandler] 群组 {} - {} 已停用", groupId, commandType);
+            bot.sendGroupMsg(groupId, "[Access] ⛔️已停用", false);
+        }
     }
 }
