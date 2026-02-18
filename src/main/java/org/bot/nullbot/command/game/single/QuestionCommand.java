2cf6a1e92db8e9556e5b437bb35a40d0ae7efc45
Zincoid <2660181154@qq.com>
2026-02-18 15:31:34 +0800

    NullBot 后端SpringBoot工程

--- src/main/java/org/bot/nullbot/command/game/single/QuestionCommand.java
+++ src/main/java/org/bot/nullbot/command/game/single/QuestionCommand.java
@@ -13,7 +13,9 @@ import org.bot.nullbot.exception.NullBotLogException;
 import org.bot.nullbot.exception.NullBotMsgException;
 import org.springframework.stereotype.Component;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -27,6 +29,7 @@ public class QuestionCommand implements Command
     private final DeepSeekClient deepSeekClient;
     private final BotNextInputer botNextInputer;
 
+    private final Set<Long> inGameUsers = new HashSet<>();
     private static final int QUESTION_TIMEOUT = 30;  // Second
 
     @Override
@@ -34,46 +37,56 @@ public class QuestionCommand implements Command
         if (event.getEvent() instanceof GroupMessageEvent groupMessageEvent) {
             Long groupId = groupMessageEvent.getGroupId();
             Long userId = groupMessageEvent.getUserId();
+            String userName = bot.getStrangerInfo(userId, true).getData().getNickname();
             List<String> params = event.getCommandParameters();
 
-            bot.sendGroupMsg(groupId, "⏳问题生成中, 请稍候...", false);
-            String raw;
+            if (inGameUsers.contains(userId))
+                throw new NullBotMsgException("[问答] ❌已在游戏中");
+
             try {
-                raw = deepSeekClient.chatSingle(
-                        "出一道单选题并给出题目和答案,问题主题:%s,生成种子:%s (注:将答案用{}包围放在开头,例如{正确选项字母},无需答案解析)"
-                        .formatted(params.isEmpty() ? "二次元" : params.getFirst(), UUID.randomUUID()),
-                        true, 2500
-                );
-            } catch (Exception e) {
-                throw new NullBotMsgException("[问答] ❌生成请求出错");
+                inGameUsers.add(userId);
+                bot.sendGroupMsg(groupId, "⏳问题生成中, 请稍候...", false);
+                String raw;
+                try {
+                    raw = deepSeekClient.chatSingle(
+                            "出一道单选题并给出题目和答案,问题主题:%s,生成种子:%s (注:将答案用{}包围放在开头,例如{正确选项字母},无需答案解析)"
+                            .formatted(params.isEmpty() ? "二次元" : params.getFirst(), UUID.randomUUID()),
+                            true, 2500
+                    );
+                } catch (Exception e) {
+                    throw new NullBotMsgException("[问答] ❌生成请求出错");
+                }
+
+                log.info("[Question] generated: {}", raw);
+
+                Pattern answerPattern = Pattern.compile("\\{([A-Za-z])}");
+                Matcher answerMatcher = answerPattern.matcher(raw);
+
+                if (answerMatcher.find()) {
+                    String answer = answerMatcher.group(1).toUpperCase();
+                    String question = """
+                            请%s(%s)回答！
+                            %s
+                            注: 请直接回复选项, 限时%s秒！"""
+                            .formatted(userName, userId, raw.replaceFirst("\\{[A-Za-z]}\\s*", ""), QUESTION_TIMEOUT);
+                    String response;
+
+                    bot.sendGroupMsg(groupId, question, false);
+                    String next = botNextInputer.request(userId, QUESTION_TIMEOUT);
+
+                    if (next == null)
+                        response = "%s回答超时！答案是...%s！".formatted(userName, answer);
+                    else if (answer.equals(next.toUpperCase()))
+                        response = "%s回答正确！".formatted(userName);
+                    else
+                        response = "%s回答错误！答案是...%s！".formatted(userName, answer);
+
+                    bot.sendGroupMsg(groupId, response, false);
+                } else
+                    throw new NullBotMsgException("[问答] ❌生成问题异常");
+            } finally {
+                inGameUsers.remove(userId);
             }
-
-            log.info("[Question] generated: {}", raw);
-
-            Pattern answerPattern = Pattern.compile("\\{([A-Za-z])}");
-            Matcher answerMatcher = answerPattern.matcher(raw);
-
-            if (answerMatcher.find()) {
-                String answer = answerMatcher.group(1).toUpperCase();
-                String question = """
-                        %s
-                        注: 请直接回复选项！限时%s秒！"""
-                        .formatted(raw.replaceFirst("\\{[A-Za-z]}\\s*", ""), QUESTION_TIMEOUT);
-                String response;
-
-                bot.sendGroupMsg(groupId, question, false);
-                String next = botNextInputer.request(userId, QUESTION_TIMEOUT);
-
-                if (next == null)
-                    response = "超时啦！答案是...%s！".formatted(answer);
-                else if (answer.equals(next.toUpperCase()))
-                    response = "回答正确！";
-                else
-                    response = "回答错误！答案是...%s！".formatted(answer);
-
-                bot.sendGroupMsg(groupId, response, false);
-            } else
-                throw new NullBotMsgException("[问答] ❌生成问题异常");
         }else
             throw new NullBotLogException("[问答] ❌未设计 - 非群消息事件响应方式");
     }
