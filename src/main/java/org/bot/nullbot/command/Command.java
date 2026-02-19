4367c41fbee39cf8d79805c1731582fcc56cbea5
Zincoid <2660181154@qq.com>
2026-02-19 23:44:06 +0800

    NullBot 后端SpringBoot工程

--- src/main/java/org/bot/nullbot/command/Command.java
+++ src/main/java/org/bot/nullbot/command/Command.java
@@ -11,13 +11,10 @@ import java.util.List;
 public interface Command
 {
     default void execute(Bot bot, GroupMessageEvent event, List<String> params) throws Exception { throw new NullBotLogException("暂无群消息事件响应方式"); }
-
     default void execute(Bot bot, PokeNoticeEvent event, List<String> params) throws Exception { throw new NullBotLogException("暂无群戳戳事件响应方式"); }
-
     default void execute(Bot bot, GroupMsgDeleteNoticeEvent event, List<String> params) throws Exception { throw new NullBotLogException("暂无群撤回事件响应方式"); }
 
     default String getHelp() { return "暂无帮助文档"; }
-
     default String getHelpForAI() { return getHelp(); }
 
     default Integer getAccess() { return 0; }
