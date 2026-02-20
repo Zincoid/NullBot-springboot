7fb28f876e1b5c0e56884abfbb8dbe4700dd9ed7
Zincoid <2660181154@qq.com>
2026-02-20 14:44:50 +0800

    NullBot 后端SpringBoot工程

--- src/main/java/org/bot/nullbot/dispatcher/handler/impl/PermissionHandler.java
+++ src/main/java/org/bot/nullbot/dispatcher/handler/impl/PermissionHandler.java
@@ -15,6 +15,8 @@ import org.bot.nullbot.service.UserService;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -26,7 +28,10 @@ import java.util.concurrent.ConcurrentHashMap;
 @Slf4j
 public class PermissionHandler implements Handler
 {
-    private final Map<Long, List<String>> bannedCmds = new ConcurrentHashMap<>();  // GroupId -> Commands
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yy-MM-dd HH:mm:ss");
+
+    private final Map<Long, List<String>> bannedCmds = new ConcurrentHashMap<>();  // GroupId -> CommandClasses
+    private final Map<String, LocalDateTime> bannedUsers = new ConcurrentHashMap<>();  // UserId + CommandClass -> BanUntil
     private boolean inMaintenance = false;
 
     private final GroupService groupService;
@@ -97,32 +102,96 @@ public class PermissionHandler implements Handler
                     - 你的限权等级: %s""".formatted(userAccess), false);
                 return;
             }
-            boolean banned = switchCmdBanning(groupId, commandClass);
+            boolean banned = switchCmdBan(groupId, commandClass);
             log.info("\t\t├─[PermissionHandler] 群组 {} - {} {}", groupId, commandClass, banned ? "已停用" : "已启用");
             bot.sendGroupMsg(groupId, "[访问] %s".formatted(banned ? "⛔️已停用" : "✅已启用"), false);
             return;
         }
 
-        if (bannedCmds.computeIfAbsent(groupId, k -> new ArrayList<>()).contains(commandClass)) {
+        if (isCmdBanned(groupId, commandClass)) {
             log.info("\t\t├─[PermissionHandler] 群组 {} - {} 停用中", groupId, commandClass);
             bot.sendGroupMsg(groupId, "[访问] ⛔️停用中", false);
+            return;
+        }
+
+        if (!params.isEmpty() && "-b".equals(params.getFirst())) {
+            if (userAccess < 1) {
+                log.info("\t\t├─[PermissionHandler] 封禁限权不足");
+                bot.sendGroupMsg(groupId, """
+                        [访问] 🚫限权不足
+                        - 需要限权等级: 1
+                        - 你的限权等级: %s""".formatted(userAccess), false);
+                return;
+            }
+            if (params.size() < 3) {
+                log.info("\t\t├─[PermissionHandler] 群组 {} - 封禁参数不足", groupId);
+                bot.sendGroupMsg(groupId, "[访问] ❌封禁参数不足", false);
+                return;
+            }
+            long targetId;
+            int banTime;
+            try {
+                targetId = Long.parseLong(params.get(1));
+                banTime = Integer.parseInt(params.get(2));
+            } catch (NumberFormatException e) {
+                log.info("\t\t├─[PermissionHandler] 群组 {} - 封禁参数非法", groupId);
+                bot.sendGroupMsg(groupId, "[访问] ❌封禁参数非法", false);
+                return;
+            }
+            setUserBan(targetId, commandClass, banTime);
+            log.info("\t\t├─[PermissionHandler] 用户 {} - {} 已封禁 {} Min", targetId, commandClass, banTime);
+            bot.sendGroupMsg(groupId, "[访问] ⛔️已封禁用户指令", false);
+            return;
+        }
+
+        if (isUserBanned(userId, commandClass)) {
+            String until = getUserBannedUntil(userId, commandClass).format(formatter);
+            log.info("\t\t├─[PermissionHandler] 用户 {} - {} 停用中", userId, commandClass);
+            bot.sendGroupMsg(groupId, """
+                    [访问] ⛔️你已被禁用该指令！
+                    - 解封于 %s""".formatted(until), false);
             return;
         }
 
         chain.doHandle(bot, event, command);
     }
 
-    // =================== 工具方法 ===================
+    // =================== 封禁方法 ===================
 
-    private boolean switchCmdBanning(Long groupId, String commandClass) {
-        List<String> bannedCmds = this.bannedCmds.computeIfAbsent(groupId, k -> new ArrayList<>());
-        if (bannedCmds.contains(commandClass)) {
-            bannedCmds.remove(commandClass);
+    private boolean switchCmdBan(Long groupId, String commandClass) {
+        List<String> banList = bannedCmds.computeIfAbsent(groupId, k -> new ArrayList<>());
+        if (banList.contains(commandClass)) {
+            banList.remove(commandClass);
             return false;
         } else {
-            bannedCmds.add(commandClass);
+            banList.add(commandClass);
             return true;
         }
+    }
+
+    private boolean isCmdBanned(Long groupId, String commandClass) {
+        return bannedCmds.computeIfAbsent(groupId, k -> new ArrayList<>()).contains(commandClass);
+    }
+
+    private void setUserBan(Long userId, String commandClass, int time) {
+        String id = "%s-%s".formatted(userId, commandClass);
+        bannedUsers.put(id, LocalDateTime.now().plusMinutes(time));
+    }
+
+    private boolean isUserBanned(Long userId, String commandClass) {
+        String id = "%s-%s".formatted(userId, commandClass);
+        LocalDateTime banUntil = bannedUsers.get(id);
+        if (banUntil == null) return false; // 用户未被封禁
+        if (LocalDateTime.now().isAfter(banUntil)) {
+            bannedUsers.remove(id);  // 封禁时间已过 自动清理
+            return false;
+        }
+        return true;
+    }
+
+    public LocalDateTime getUserBannedUntil(Long userId, String commandClass) {
+        String id = "%s-%s".formatted(userId, commandClass);
+        return bannedUsers.get(id);
     }
 
     // =================== 锁定方法 ===================
