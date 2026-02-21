4c00f813d2847f1ce6f64c1d9bf0db9c8005219a
Zincoid <2660181154@qq.com>
2026-02-21 13:49:24 +0800

    NullBot 后端SpringBoot工程

--- src/main/java/org/bot/nullbot/command/Command.java
+++ src/main/java/org/bot/nullbot/command/Command.java
@@ -6,6 +6,7 @@ import com.mikuac.shiro.dto.event.message.PrivateMessageEvent;
 import com.mikuac.shiro.dto.event.notice.GroupMsgDeleteNoticeEvent;
 import com.mikuac.shiro.dto.event.notice.PokeNoticeEvent;
 import org.bot.nullbot.exception.NullBotLogException;
+import org.bot.nullbot.exception.NullBotMsgException;
 
 import java.util.List;
 
@@ -14,7 +15,7 @@ public interface Command
     default void execute(Bot bot, GroupMessageEvent event, List<String> params) throws Exception { throw new NullBotLogException("暂无群消息事件响应方式"); }
     default void execute(Bot bot, PokeNoticeEvent event, List<String> params) throws Exception { throw new NullBotLogException("暂无群戳戳事件响应方式"); }
     default void execute(Bot bot, GroupMsgDeleteNoticeEvent event, List<String> params) throws Exception { throw new NullBotLogException("暂无群撤回事件响应方式"); }
-    default void execute(Bot bot, PrivateMessageEvent event, List<String> params) throws Exception { throw new NullBotLogException("暂无私信事件响应方式"); }
+    default void execute(Bot bot, PrivateMessageEvent event, List<String> params) throws Exception { throw new NullBotMsgException("暂无私信事件响应方式"); }
 
     default String getHelp() { return "暂无帮助文档"; }
     default String getHelpForAI() { return getHelp(); }
