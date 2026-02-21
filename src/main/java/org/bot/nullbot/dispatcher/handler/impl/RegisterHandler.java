910958c30fc2c050f064de1e0c7f8c323dcdcbf6
Zincoid <2660181154@qq.com>
2026-02-21 15:04:56 +0800

    NullBot 后端SpringBoot工程

--- src/main/java/org/bot/nullbot/dispatcher/handler/impl/RegisterHandler.java
+++ src/main/java/org/bot/nullbot/dispatcher/handler/impl/RegisterHandler.java
@@ -38,7 +38,7 @@ public class RegisterHandler implements Handler
             groupId = groupMessageEvent.getGroupId();
             userId = groupMessageEvent.getUserId();
         } else if (event.getEvent() instanceof PokeNoticeEvent pokeNoticeEvent) {
-            groupId = pokeNoticeEvent.getGroupId();
+            groupId = pokeNoticeEvent.getGroupId() == null ? 0L : pokeNoticeEvent.getGroupId();  // 群号 0 代表私聊
             userId = pokeNoticeEvent.getUserId();
         } else if (event.getEvent() instanceof GroupMsgDeleteNoticeEvent groupMsgDeleteNoticeEvent) {
             groupId = groupMsgDeleteNoticeEvent.getGroupId();
