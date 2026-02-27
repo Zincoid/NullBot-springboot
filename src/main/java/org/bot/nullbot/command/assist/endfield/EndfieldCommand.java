fa0082e95c7040951ee73289838961bad6aec38b
Zincoid <2660181154@qq.com>
2026-02-27 18:16:35 +0800

    NullBot 后端SpringBoot工程

--- src/main/java/org/bot/nullbot/command/assist/endfield/EndfieldCommand.java
+++ src/main/java/org/bot/nullbot/command/assist/endfield/EndfieldCommand.java
@@ -37,6 +37,8 @@ public class EndfieldCommand implements Command
     private static final int PAGE_SIZE = 10;  // 查询单页大小
     private static final int WAIT_TIMEOUT = 30;  // 等待超时时间 (单位: Second)
 
+    private boolean continuousQuery = true;  // 连续查询模式
+
     @Override
     public void execute(Bot bot, GroupMessageEvent event, List<String> params) {
         Long groupId = event.getGroupId();
@@ -53,6 +55,7 @@ public class EndfieldCommand implements Command
         int current = 1;
 
         String operation = "INIT";
+        boolean sendPage = true;
         while (true) {
             switch (operation) {
                 case "INIT" -> {
@@ -78,28 +81,12 @@ public class EndfieldCommand implements Command
                     if (i < 0 || i > total - 1)
                         throw new NullBotMsgException("[终末地] ❌范围错误");
                     sendResource(bot, groupId, helpPaths.get(i));
-                    return;
+                    if (!continuousQuery) return;
+                    sendPage = false;
                 }
             }
 
-            int fromIndex = (current - 1) * PAGE_SIZE;
-            int toIndex = Math.min(fromIndex + PAGE_SIZE, total);
-            List<String> helpNames = IntStream.range(fromIndex, toIndex)
-                    .mapToObj(j -> {
-                        String path = helpPaths.get(j);
-                        String fileNameWithExt = new File(path).getName();
-                        int dotIndex = fileNameWithExt.lastIndexOf('.');
-                        String fileName = dotIndex > 0 ? fileNameWithExt.substring(0, dotIndex) : fileNameWithExt;
-                        return (j + 1) + ". " + fileName;
-                    }).toList();
-            String content = String.join("\n", helpNames);
-            String footer = """
-                    [第 %s/%s 页 (每页%s条)]
-                    翻页 - Up/Down
-                    选择 - 发送条目序号""".formatted(current, pages, PAGE_SIZE);
-            bot.sendGroupMsg(groupId, "[终末地] \uD83D\uDD0D共%s个结果\n%s\n\n%s"
-                    .formatted(total, content, footer), false);
-            log.info("\t\t\t\t├─[Endfield] 已获取查询页 - {}/{}", current, pages);
+            if (sendPage) sendPage(bot, groupId, helpPaths, PAGE_SIZE, current);
 
             List<Pair<Long, String>> inputs;
             try {
@@ -112,23 +99,45 @@ public class EndfieldCommand implements Command
             if (inputs.isEmpty())
                 throw new NullBotMsgException("[终末地] ⌛️输入超时");
             operation = inputs.getFirst().getRight().toUpperCase();
+            sendPage = true;
         }
+    }
+
+    private void sendPage(Bot bot, Long groupId, List<String> helpPaths, int pageSize, int current) {
+        int total = helpPaths.size();
+        int pages = (total + pageSize - 1) / pageSize;
+        int fromIndex = (current - 1) * pageSize;
+        int toIndex = Math.min(fromIndex + pageSize, total);
+        List<String> helpNames = IntStream.range(fromIndex, toIndex)
+                .mapToObj(j -> {
+                    String path = helpPaths.get(j);
+                    String fileNameWithExt = new File(path).getName();
+                    int dotIndex = fileNameWithExt.lastIndexOf('.');
+                    String fileName = dotIndex > 0 ? fileNameWithExt.substring(0, dotIndex) : fileNameWithExt;
+                    return (j + 1) + ". " + fileName;
+                }).toList();
+        String content = String.join("\n", helpNames);
+        String footer = """
+                [第 %s/%s 页 (每页%s条)]
+                翻页 - Up/Down
+                选择 - 发送条目序号""".formatted(current, pages, pageSize);
+        bot.sendGroupMsg(groupId, "[终末地] \uD83D\uDD0D共%s个结果\n%s\n\n%s"
+                .formatted(total, content, footer), false);
+        log.info("\t\t\t\t├─[Endfield] 已获取查询页 - {}/{}", current, pages);
     }
 
     private void sendResource(Bot bot, Long groupId, String resourcePath) {
         String helpName = new File(resourcePath).getName().toLowerCase();
         if (helpName.endsWith(".txt")) {
-            // TXT文件类型 读取文本内容
             try {
-                String response = Files.readString(Paths.get(resourcePath), StandardCharsets.UTF_8);
+                String response = Files.readString(Paths.get(resourcePath), StandardCharsets.UTF_8);  // TXT文件类型 读取文本内容
                 bot.sendGroupMsg(groupId, response, false);
                 log.info("\t\t\t\t├─[Endfield] 已获取文本内容");
             } catch (IOException e) {
                 throw new NullBotMsgException("[终末地] ❌读取出错");
             }
         } else {
-            // 其他文件类型 按图片处理
-            String response = MsgUtils.builder().img(resourcePath).build();
+            String response = MsgUtils.builder().img(resourcePath).build();  // 其他文件类型 按图片处理
             bot.sendGroupMsg(groupId, response, false);
             log.info("\t\t\t\t├─[Endfield] 已获取图片内容");
         }
