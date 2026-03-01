e2bb6a4914e845c9c0d90255b97ea0c7c3b91722
Zincoid <2660181154@qq.com>
2026-03-01 21:55:48 +0800

    NullBot 后端SpringBoot工程

--- src/main/java/org/bot/nullbot/component/tool/Restarter.java
+++ src/main/java/org/bot/nullbot/component/tool/Restarter.java
@@ -4,7 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.bot.nullbot.NullBotApplication;
 import org.springframework.stereotype.Component;
 
-import java.io.File;
+import java.io.*;
 
 @Component
 @Slf4j
@@ -12,6 +12,7 @@ public class Restarter
 {
     private static final String SCREEN_SESSION_NAME = "nullbot";
     private static final String DEFAULT_JAR_PATH = "/root/Nullbot/jar/NullBot-springboot-0.0.1-SNAPSHOT.jar";
+    private static final String JAVA_BIN = "java";
 
     public void restart() {
         log.info("▽ [Restarter] 正在重启应用...");
@@ -32,26 +33,29 @@ public class Restarter
                 throw new IllegalArgumentException("JAR文件不存在");
             }
 
-            // 构建 重启命令
-            // String javaBin = System.getProperty("java.home") + File.separator + "bin" + File.separator + "java";
-            String javaBin = "java";
-            String javaCmd = "%s -jar %s".formatted(javaBin, jarPath);
+            // 构建 启动命令
+            String javaCmd = "%s -jar %s".formatted(JAVA_BIN, jarPath);
             String[] runScreenCmd = {"screen", "-dmS", SCREEN_SESSION_NAME, "bash", "-c", javaCmd};
-            String[] killScreenCmd = {"screen", "-S", SCREEN_SESSION_NAME, "-X", "quit"};
 
-            // 执行 重启命令
+            // 启动 新的进程
             log.info("▽ [Restarter] 将于3秒后重启...");
             Thread.sleep(3000);
-            new ProcessBuilder(killScreenCmd).start().waitFor();
-            Thread.sleep(1000);
-            new ProcessBuilder(runScreenCmd).start().waitFor();
-            Thread.sleep(1000);
+            if (new ProcessBuilder(runScreenCmd).start().waitFor() == 0) {
+                log.info("▽ [Restarter] 新进程启动成功");
+            } else {
+                log.info("▽ [Restarter] 新进程启动失败");
+                throw new RuntimeException("新进程启动失败");
+            }
+
+            // 退出 当前进程
+            log.info("▽ [Restarter] 将于3秒后退出...");
+            Thread.sleep(3000);
             System.exit(0);
 
-        } catch (IllegalArgumentException e) {
+        } catch (RuntimeException e) {
             throw e;
         } catch (Exception e) {
-            log.error("▽ [Restarter] 重启过程中出错", e);
+            log.error("▽ [Restarter] 重启时捕获未知非运行异常", e);
         }
     }
 }
