8d0ac96fabd35b61e0aa0532c5aafd271f631578
Zincoid <2660181154@qq.com>
2026-03-01 17:13:00 +0800

    NullBot 后端SpringBoot工程

---
+++ src/main/java/org/bot/nullbot/command/system/LogCommand.java
@@ -0,0 +1,43 @@
+package org.bot.nullbot.command.system;
+
+import com.mikuac.shiro.core.Bot;
+import com.mikuac.shiro.dto.event.message.GroupMessageEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.bot.nullbot.annotation.CommandMapping;
+import org.bot.nullbot.command.Command;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@CommandMapping({"Log", "日志"})
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class LogCommand implements Command
+{
+    @Override
+    public void execute(Bot bot, GroupMessageEvent event, List<String> params) {
+        bot.uploadGroupFile(
+                event.getGroupId(),
+                "/root/Nullbot/output.log",
+                "Log_%s.log".formatted(LocalDateTime.now())
+        );
+        log.info("\t\t\t\t├─[Log] 日志已发送");
+    }
+
+    @Override
+    public Integer getAccess() { return 2; }
+
+    @Override
+    public String getHelp() {
+        return String.format("""
+                ◉ Log 命令
+                功能: 发送日志文件
+                限权: %d 级
+                格式: Log
+                别名: 日志""", getAccess()
+        );
+    }
+}
