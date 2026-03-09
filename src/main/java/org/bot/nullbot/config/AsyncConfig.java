7a4d5e4c9f83ae16c70d100e2c5c96569146efbf
Zincoid <2660181154@qq.com>
2026-03-09 18:04:19 +0800

    NullBot 后端SpringBoot工程

--- src/main/java/org/bot/nullbot/config/AsyncConfig.java
+++ src/main/java/org/bot/nullbot/config/AsyncConfig.java
@@ -7,13 +7,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-import org.springframework.stereotype.Component;
 
 import java.lang.reflect.Method;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-@Component
 @EnableAsync
 @Configuration
 @Slf4j
