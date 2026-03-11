83644219bc883f03769b1c340bb1af37d3f550b8
Zincoid <2660181154@qq.com>
2026-03-11 18:56:32 +0800

    NullBot 后端SpringBoot工程

---
+++ src/main/java/org/bot/nullbot/websocket/WebSocketBroadcastService.java
@@ -0,0 +1,22 @@
+package org.bot.nullbot.websocket;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class WebSocketBroadcastService
+{
+    private final SimpMessagingTemplate messagingTemplate;
+
+    /**
+     * 广播日志消息 - /topic/log
+     * @param level  日志级别
+     * @param message 日志内容
+     */
+    public void broadcast(String level, String message) {
+        String json = String.format("{\"level\":\"%s\",\"message\":\"%s\"}", level, message);
+        messagingTemplate.convertAndSend("/topic/log", json);
+    }
+}
