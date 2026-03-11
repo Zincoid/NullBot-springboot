931a7148e20345c2f9dd742f5285af35808dc520
Zincoid <2660181154@qq.com>
2026-03-12 01:51:48 +0800

    NullBot 后端SpringBoot工程

--- src/main/java/org/bot/nullbot/websocket/WebSocketInterceptor.java
+++ src/main/java/org/bot/nullbot/websocket/WebSocketInterceptor.java
@@ -38,44 +38,33 @@ public class WebSocketInterceptor implements ChannelInterceptor
         if (accessor != null && StompCommand.CONNECT.equals(accessor.getCommand())) {
             String ip = (String) accessor.getSessionAttributes().get("clientIp");
             log.info("◎ [WebSocketManagement] 来自 {} 的连接请求", ip);
-            // 从 Header 中获取 Token (例如 token: Bearer <token>)
+            // 从 Header 中获取 Token (token: Bearer <token>)
             String authHeader = accessor.getFirstNativeHeader("token");
             if (!StringUtils.hasText(authHeader) || !authHeader.startsWith("Bearer "))
                 throw new IllegalArgumentException("No Token");
             String token = authHeader.substring(7);
             // 验证 Token
-            if (validateToken(token)) {
-                Long userId = extractUserId(token);
-                AdminPO admin = adminService.info(userId);
-                if (admin == null) {
-                    log.info("└─[WebSocketInterceptor] 管理员不存在");
-                    throw new IllegalArgumentException("Invalid Admin");
-                }
-                StompPrincipal user = new StompPrincipal(userId, admin.getUsername());
-                // 将 Principal 设置到 StompHeaderAccessor 中
-                // 后续可以在 @MessageMapping 方法中通过 Principal 参数获取
-                accessor.setUser(user);
-                log.info("└─[WebSocketInterceptor] 连接放行 - UserId: {}", userId);
-                return message;
-            } else {
+            JWT jwt;
+            try {
+                jwt = jwtTool.parseJwt(token);
+            } catch (Exception e) {
                 log.info("└─[WebSocketInterceptor] 验证失败");
                 throw new IllegalArgumentException("Invalid Token");
             }
+            Long userId = jwtTool.getAs(jwt, "id", Long.class);
+            AdminPO admin = adminService.info(userId);
+            if (admin == null) {
+                log.info("└─[WebSocketInterceptor] 管理员不存在");
+                throw new IllegalArgumentException("Invalid Admin");
+            }
+            StompPrincipal user = new StompPrincipal(userId, admin.getUsername());
+            // 将 Principal 设置到 StompHeaderAccessor 中
+            // 后续可以在 @MessageMapping 方法中通过 Principal 参数获取
+            accessor.setUser(user);
+            log.info("└─[WebSocketInterceptor] 连接放行 - UserId: {}", userId);
+            return message;
         }
         log.info("└─[WebSocketInterceptor] 默认放行");
         return message;
-    }
-
-    private boolean validateToken(String token) {
-        try {
-            jwtTool.parseJwt(token);
-        } catch (Exception e) {
-            return false;
-        }
-        return true;
-    }
-
-    private Long extractUserId(String token) {
-        return jwtTool.getLoginId(token);
     }
 }
