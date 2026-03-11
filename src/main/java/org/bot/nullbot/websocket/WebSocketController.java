1360ce9f6498bf4daf4a8e98fa285457a4979a86
Zincoid <2660181154@qq.com>
2026-03-11 18:56:38 +0800

    NullBot 后端SpringBoot工程

---
+++ src/main/java/org/bot/nullbot/websocket/WebSocketController.java
@@ -0,0 +1,16 @@
+package org.bot.nullbot.websocket;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.stereotype.Controller;
+
+@Slf4j
+@Controller
+public class WebSocketController
+{
+    @MessageMapping("/command")  // 客户端发送到 /monitor/command 的消息会由这个方法处理
+    public void handleCommand(String command) {
+        log.info("◉ [WebSocketController] 接收到客户端命令 - {}", command);
+        // 处理命令...
+    }
+}
