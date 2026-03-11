69d2a230b11ee0e8b292223004658e016ae030cb
Zincoid <2660181154@qq.com>
2026-03-11 18:46:35 +0800

    NullBot 后端SpringBoot工程

---
+++ src/main/java/org/bot/nullbot/websocket/WebSocketListener.java
@@ -0,0 +1,25 @@
+package org.bot.nullbot.websocket;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionConnectedEvent;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+@Slf4j
+@Component
+public class WebSocketListener
+{
+    @EventListener
+    public void handleWebSocketConnectListener(SessionConnectedEvent event) {
+        StompHeaderAccessor headers = StompHeaderAccessor.wrap(event.getMessage());
+        log.info("▽ [WebSocketListener] 连接已建立 - SessionID: {}", headers.getSessionId());
+    }
+
+    @EventListener
+    public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
+        StompHeaderAccessor headers = StompHeaderAccessor.wrap(event.getMessage());
+        log.info("▽ [WebSocketListener] 连接已断开 - SessionID: {}", headers.getSessionId());
+    }
+}
