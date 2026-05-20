c6fe3b0d7911c697fe1b03a7171d66318bbcc13d
Zincoid <2660181154@qq.com>
2026-05-20 18:42:52 +0800

    NullBot 后端SpringBoot工程

--- src/main/java/org/bot/nullbot/interceptor/BotInterceptor.java
+++ src/main/java/org/bot/nullbot/interceptor/BotInterceptor.java
@@ -11,10 +11,12 @@ public class BotInterceptor implements BotMessageEventInterceptor {
 
     @Override
     public boolean preHandle(Bot bot, MessageEvent event) throws ShiroException {
+        // 暂时不用
         return true;
     }
 
     @Override
     public void afterCompletion(Bot bot, MessageEvent event) throws ShiroException {
+        // 暂时不用
     }
 }
