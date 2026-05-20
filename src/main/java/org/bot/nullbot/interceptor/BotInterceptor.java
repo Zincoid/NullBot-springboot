5822bf5a0a59b51880270ae6a4aedb62fe95de13
Zincoid <2660181154@qq.com>
2026-05-20 17:32:59 +0800

    NullBot 后端SpringBoot工程

---
+++ src/main/java/org/bot/nullbot/interceptor/BotInterceptor.java
@@ -0,0 +1,18 @@
+package org.bot.nullbot.interceptor;
+
+import com.mikuac.shiro.core.Bot;
+import com.mikuac.shiro.core.BotMessageEventInterceptor;
+import com.mikuac.shiro.dto.event.message.MessageEvent;
+import com.mikuac.shiro.exception.ShiroException;
+
+public class BotInterceptor implements BotMessageEventInterceptor {
+
+    @Override
+    public boolean preHandle(Bot bot, MessageEvent event) throws ShiroException {
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(Bot bot, MessageEvent event) throws ShiroException {
+    }
+}
