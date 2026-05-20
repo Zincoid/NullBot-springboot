6dfc8807a377cc664dad64603a18e43e0ae11454
Zincoid <2660181154@qq.com>
2026-05-20 16:27:19 +0800

    NullBot 后端SpringBoot工程

--- src/main/java/org/bot/nullbot/service/SettingService.java
+++ src/main/java/org/bot/nullbot/service/SettingService.java
@@ -1,87 +1,32 @@
 package org.bot.nullbot.service;
 
-import org.bot.nullbot.entity.ChatOption;
-import org.bot.nullbot.entity.info.SettingInfo;
-import org.bot.nullbot.enums.ChatScope;
-import org.bot.nullbot.enums.LimitScope;
+import org.bot.nullbot.entity.setting.*;
 
 import java.util.List;
 
 public interface SettingService {
 
-    SettingInfo get(Long groupId);
+    Setting get(Long groupId);
 
-    boolean set(SettingInfo setting);
+    boolean set(Setting setting);
 
-    List<SettingInfo> getList();
+    List<Setting> getAll();
 
-    void sets(List<SettingInfo> settings);
+    void setAll(List<Setting> settings);
 
-    LimitScope getLimitScope(Long groupId);
+    LimitOption getLimitOption(Long groupId);
 
-    int getLimitCapacity(Long groupId);
-
-    int getLimitRefill(Long groupId);
-
-    int getLimitInterval(Long groupId);
-
-    LimitScope switchLimitScope(Long groupId);
-
-    boolean setLimitCapacity(Long groupId, int limitCapacity);
-
-    boolean setLimitRefill(Long groupId, int limitRefill);
-
-    boolean setLimitInterval(Long groupId, int limitInterval);
+    void setLimitOption(Long groupId, LimitOption limitOption);
 
     ChatOption getChatOption(Long groupId);
 
-    boolean isAutoReply(Long groupId);
+    void setChatOption(Long groupId, ChatOption chatOption);
 
-    double getReplyFrequency(Long groupId);
+    MonitorOption getMonitorOption(Long groupId);
 
-    ChatScope switchChatScope(Long groupId);
+    void setMonitorOption(Long groupId, MonitorOption monitorOption);
 
-    boolean switchAntiInjection(Long groupId);
+    GuessOption getGuessOption(Long groupId);
 
-    boolean switchThinking(Long groupId);
-
-    boolean switchVoice(Long groupId);
-
-    boolean switchEmbedding(Long groupId);
-
-    boolean switchEmbeddingAuth(Long groupId);
-
-    boolean switchCustom(Long groupId);
-
-    boolean switchAutoReply(Long groupId);
-
-    boolean setReplyFrequency(Long groupId, double frequency);
-
-    boolean isImageCollect(Long groupId);
-
-    boolean isMessageCollect(Long groupId);
-
-    boolean isKeywordDetect(Long groupId);
-
-    boolean isPokeDetect(Long groupId);
-
-    boolean isRecallDetect(Long groupId);
-
-    boolean switchImageCollect(Long groupId);
-
-    boolean switchMessageCollect(Long groupId);
-
-    boolean switchKeywordDetect(Long groupId);
-
-    boolean switchPokeDetect(Long groupId);
-
-    boolean switchRecallDetect(Long groupId);
-
-    double getGuessCropRatio(Long groupId);
-
-    double getGuessTransparentRatio(Long groupId);
-
-    int getGuessPadding(Long groupId);
-
-    boolean setGuessParams(Long groupId, double cropRatio, double transparentRatio, int padding);
+    void setGuessOption(Long groupId, GuessOption guessOption);
 }
