782301cbecb4070170b14191e3dca6a9c3437918
Zincoid <2660181154@qq.com>
2026-05-20 16:34:32 +0800

    NullBot 后端SpringBoot工程

--- src/main/java/org/bot/nullbot/component/aspect/FunctionAspect.java
+++ src/main/java/org/bot/nullbot/component/aspect/FunctionAspect.java
@@ -20,26 +20,21 @@ public class FunctionAspect {
 
     @Around("@annotation(function)")
     public Object checkFunction(ProceedingJoinPoint joinPoint, FunctionControl function) throws Throwable {
-        boolean enabled = functionManager.isEnabled(function.value());
-        if (enabled) {
-            // logger.info("◉ [FunctionManager] {} 已启用", function.config());
+        if (functionManager.isEnabled(function.value()))
             return joinPoint.proceed();
-        } else {
-            // logger.info("◉ [FunctionManager] {} 未启用", function.config());
-            // 根据方法返回类型返回默认值
-            MethodSignature signature = (MethodSignature) joinPoint.getSignature();
-            Class<?> returnType = signature.getReturnType();
-            if (returnType.isPrimitive()) {
-                if (returnType == boolean.class) return false;
-                if (returnType == byte.class) return (byte) 0;
-                if (returnType == char.class) return '\u0000';
-                if (returnType == short.class) return (short) 0;
-                if (returnType == int.class) return 0;
-                if (returnType == long.class) return 0L;
-                if (returnType == float.class) return 0.0f;
-                if (returnType == double.class) return 0.0d;
-            }
-            return null;
+        // 根据方法返回类型返回默认值
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        Class<?> returnType = signature.getReturnType();
+        if (returnType.isPrimitive()) {
+            if (returnType == boolean.class) return false;
+            if (returnType == byte.class) return (byte) 0;
+            if (returnType == char.class) return '\u0000';
+            if (returnType == short.class) return (short) 0;
+            if (returnType == int.class) return 0;
+            if (returnType == long.class) return 0L;
+            if (returnType == float.class) return 0.0f;
+            if (returnType == double.class) return 0.0d;
         }
+        return null;
     }
 }
