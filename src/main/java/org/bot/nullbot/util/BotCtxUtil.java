e859d02ab16882095bf50533bc45566f4f0ef627
Zincoid <2660181154@qq.com>
2026-05-20 18:42:50 +0800

    NullBot 后端SpringBoot工程

--- src/main/java/org/bot/nullbot/util/BotCtxUtil.java
+++ src/main/java/org/bot/nullbot/util/BotCtxUtil.java
@@ -1,36 +1,38 @@
 package org.bot.nullbot.util;
 
+import org.bot.nullbot.entity.po.SettingPO;
+
 public final class BotCtxUtil {
 
-    public static final ThreadLocal<String> userId = new ThreadLocal<>();
-    public static final ThreadLocal<String> groupId = new ThreadLocal<>();
-    public static final ThreadLocal<String> setting = new ThreadLocal<>();
+    public static final ThreadLocal<Long> userId = new ThreadLocal<>();
+    public static final ThreadLocal<Long> groupId = new ThreadLocal<>();
+    public static final ThreadLocal<SettingPO> setting = new ThreadLocal<>();
 
     private BotCtxUtil() {}
 
-    public static void set(String userId, String groupId, String setting) {
+    public static void set(Long userId, Long groupId, SettingPO setting) {
         setUserId(userId);
         setGroupId(groupId);
         setSetting(setting);
     }
 
-    public static void setUserId(String userId) {
+    public static void setUserId(Long userId) {
         BotCtxUtil.userId.set(userId);
     }
-    public static void setGroupId(String groupId) {
+    public static void setGroupId(Long groupId) {
         BotCtxUtil.groupId.set(groupId);
     }
-    public static void setSetting(String setting) {
+    public static void setSetting(SettingPO setting) {
         BotCtxUtil.setting.set(setting);
     }
 
-    public static String getUserId() {
+    public static Long getUserId() {
         return userId.get();
     }
-    public static String getGroupId() {
+    public static Long getGroupId() {
         return groupId.get();
     }
-    public static String getSetting() {
+    public static SettingPO getSetting() {
         return setting.get();
     }
 
