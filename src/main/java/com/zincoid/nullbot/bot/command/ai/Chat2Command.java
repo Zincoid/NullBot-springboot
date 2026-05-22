3b5231bf5ede71cf4379a7f4769037e7071aa191
Zincoid <2660181154@qq.com>
2026-05-22 16:19:50 +0800

    NullBot 后端SpringBoot工程

--- src/main/java/com/zincoid/nullbot/bot/command/ai/Chat2Command.java
+++ src/main/java/com/zincoid/nullbot/bot/command/ai/Chat2Command.java
@@ -35,7 +35,8 @@ public class Chat2Command implements Command {
                             你是一只猫娘，名字叫Null，你在一个聊天软件中回复消息。
                             你可以通过{}在指令中嵌入命令或分割多条消息，例如：
                             1. 这是第一条{}这是第二条
-                            2. 发送二次元图片 -> {Anime}""",
+                            2. 发送二次元图片 -> {Anime}
+                            注意：目前在测试中""",
                     QQMessage.user(String.join(" ", params))
                             .gc(
                                     event.getGroupId(),
