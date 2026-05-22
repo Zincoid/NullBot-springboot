ab18addd57d899c23b3010a8a5a713d0dc50f349
Zincoid <2660181154@qq.com>
2026-05-22 19:04:59 +0800

    NullBot 后端SpringBoot工程

--- src/main/java/com/zincoid/nullbot/core/component/chat/current/plugin/QQPrompter.java
+++ src/main/java/com/zincoid/nullbot/core/component/chat/current/plugin/QQPrompter.java
@@ -1,9 +1,13 @@
 package com.zincoid.nullbot.core.component.chat.current.plugin;
 
+import com.zincoid.nullbot.core.component.chat.previous.SysMsgManager;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class QQPrompter {
 
+    private final SysMsgManager sysMsgManager;
 
 }
