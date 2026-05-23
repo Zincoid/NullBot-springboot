3eb30345577dea7a94e3389111b3e21d30a8a94d
Zincoid <2660181154@qq.com>
2026-05-24 02:14:11 +0800

    NullBot 后端SpringBoot工程

--- src/main/java/com/zincoid/nullbot/core/component/ai/chat/model/OpenAiModel.java
+++ src/main/java/com/zincoid/nullbot/core/component/ai/chat/model/OpenAiModel.java
@@ -2,7 +2,6 @@ package com.zincoid.nullbot.core.component.ai.chat.model;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.NullNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.zincoid.nullbot.core.component.ai.chat.message.Message;
 import com.zincoid.nullbot.core.properties.OpenAiProperties;
