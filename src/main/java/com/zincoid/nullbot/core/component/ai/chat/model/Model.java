eb5bd82fcf946d876942519d0147e823a655bc62
Zincoid <2660181154@qq.com>
2026-05-24 15:36:09 +0800

    NullBot 后端SpringBoot工程

--- src/main/java/com/zincoid/nullbot/core/component/ai/chat/model/Model.java
+++ src/main/java/com/zincoid/nullbot/core/component/ai/chat/model/Model.java
@@ -1,10 +1,15 @@
 package com.zincoid.nullbot.core.component.ai.chat.model;
 
 import com.zincoid.nullbot.core.component.ai.chat.message.Message;
+import com.zincoid.nullbot.core.component.ai.chat.tool.ToolDef;
 
 import java.util.List;
 
 public interface Model {
 
-    public Message invoke(List<Message> messages, boolean thinking, int maxTokens);
+    ModelResponse invoke(List<Message> messages, boolean thinking, int maxTokens);
+
+    default ModelResponse invoke(List<Message> messages, List<ToolDef> tools, boolean thinking, int maxTokens) {
+        return invoke(messages, thinking, maxTokens);
+    }
 }
