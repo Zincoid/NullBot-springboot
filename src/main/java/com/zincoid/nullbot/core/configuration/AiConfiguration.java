d7eaeb9b2f26c23ceea3243fadb5ab16cc4ddd10
Zincoid <2660181154@qq.com>
2026-05-24 18:44:41 +0800

    NullBot 后端SpringBoot工程

--- src/main/java/com/zincoid/nullbot/core/configuration/AiConfiguration.java
+++ src/main/java/com/zincoid/nullbot/core/configuration/AiConfiguration.java
@@ -14,7 +14,6 @@ import com.zincoid.nullbot.core.properties.AiChatProperties;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
 
 @Slf4j
 @Configuration
