0d5fc695431dc0f5bec4edc14d08f0953fe8e5a0
Zincoid <2660181154@qq.com>
2026-05-24 15:37:37 +0800

    NullBot 后端SpringBoot工程

--- src/main/java/com/zincoid/nullbot/core/component/ai/chat/tool/ToolCall.java
+++ src/main/java/com/zincoid/nullbot/core/component/ai/chat/tool/ToolCall.java
@@ -2,10 +2,8 @@ package com.zincoid.nullbot.core.component.ai.chat.tool;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
 @AllArgsConstructor
 public class ToolCall {
 
