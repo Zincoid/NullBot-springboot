a556e1fb69eba443cbfa3380e4559fbec8f1fc87
Zincoid <2660181154@qq.com>
2026-05-25 16:50:28 +0800

    NullBot 后端SpringBoot工程

--- src/main/java/com/zincoid/nullbot/bot/dispatcher/CommandProcessor.java
+++ src/main/java/com/zincoid/nullbot/bot/dispatcher/CommandProcessor.java
@@ -6,7 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import com.zincoid.nullbot.bot.command.Command;
 import com.zincoid.nullbot.bot.dispatcher.handler.Handler;
 import com.zincoid.nullbot.core.model.bot.event.CommandEvent;
-import com.zincoid.nullbot.core.model.bot.event.AiCommandEvent;
+import com.zincoid.nullbot.core.model.bot.event.InnerCommandEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
@@ -20,7 +20,7 @@ public class CommandProcessor {
     private final CommandRegistry registry;
     private final List<Handler> handlers;
 
-    // 处理 QQ 指令
+    // 处理用户指令
     public void processQQ(Bot bot, CommandEvent<?> event) throws Exception {
         Command command = registry.getCommand(event.getCommandType());
         if (command != null) {
@@ -31,19 +31,19 @@ public class CommandProcessor {
             log.info("■ [CommandProcessor::QQ] 命令不存在");
     }
 
-    // 处理 AI 指令
+    // 处理内部指令
     @EventListener
-    public void processAI(AiCommandEvent event) throws Exception {
+    public void processInner(InnerCommandEvent event) throws Exception {
         Command command = registry.getCommand(event.getEvent().getCommandType());
         if (command != null) {
-            log.info("\t\t▶ [CommandProcessor::AI] 正在处理 {} 命令...", event.getEvent().getCommandType());
+            log.info("\t\t▶ [CommandProcessor::Inner] 正在处理 {} 命令...", event.getEvent().getCommandType());
             chainProcess(event.getBot(), event.getEvent(), command);
-            log.info("\t\t■ [CommandProcessor::AI] {} 命令处理完毕", event.getEvent().getCommandType());
+            log.info("\t\t■ [CommandProcessor::Inner] {} 命令处理完毕", event.getEvent().getCommandType());
         } else
-            log.info("\t\t■ [CommandProcessor::AI] 命令不存在");
+            log.info("\t\t■ [CommandProcessor::Inner] 命令不存在");
     }
 
-    // 处理 测试指令
+    // 处理测试指令
     public void processTest(CommandEvent<?> event) throws Exception {
         Command command = registry.getCommand(event.getCommandType());
         if (command != null) {
---
+++ src/main/java/com/zincoid/nullbot/core/model/bot/event/InnerCommandEvent.java
@@ -0,0 +1,30 @@
+package com.zincoid.nullbot.core.model.bot.event;
+
+import com.mikuac.shiro.core.Bot;
+import com.zincoid.nullbot.core.util.BotCtxUtil;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class InnerCommandEvent {
+
+    private Bot bot;
+    private CommandEvent<?> event;
+
+    public static InnerCommandEvent of(String command) {
+        return new InnerCommandEvent(
+                BotCtxUtil.getBot(),
+                new CommandEvent<>(BotCtxUtil.getEvent(), command,
+                        !BotCtxUtil.getIsPrivate() && BotCtxUtil.getSetting().isEmbeddingAuth(),
+                        false)
+        );
+    }
+
+    public static InnerCommandEvent of(String command, boolean authRequired) {
+        return new InnerCommandEvent(
+                BotCtxUtil.getBot(),
+                new CommandEvent<>(BotCtxUtil.getEvent(), command, authRequired, false)
+        );
+    }
+}
