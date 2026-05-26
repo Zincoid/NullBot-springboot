588a76b63f1149e2a89a19fcf850fab885b05a53
Zincoid <2660181154@qq.com>
2026-05-26 19:35:45 +0800

    NullBot 后端SpringBoot工程

--- src/main/java/com/zincoid/nullbot/bot/command/assist/endfield/EssencePlanCommand.java
+++ src/main/java/com/zincoid/nullbot/bot/command/assist/endfield/EssencePlanCommand.java
@@ -3,50 +3,40 @@ package com.zincoid.nullbot.bot.command.assist.endfield;
 import com.mikuac.shiro.common.utils.MsgUtils;
 import com.mikuac.shiro.core.Bot;
 import com.mikuac.shiro.dto.event.message.GroupMessageEvent;
+import com.zincoid.nullbot.bot.command.CommandArgs;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import com.zincoid.nullbot.core.annotation.CommandMapping;
 import com.zincoid.nullbot.bot.command.Command;
 import com.zincoid.nullbot.core.component.render.WebScreenCapturer;
-import com.zincoid.nullbot.bot.exception.NullBotException;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Slf4j
 @CommandMapping({"EssencePlan", "基质规划", "基质"})
 @Component
-@Slf4j
 @RequiredArgsConstructor
 public class EssencePlanCommand implements Command {
 
     private final WebScreenCapturer webScreenCapturer;
 
     @Override
-    public void execute(Bot bot, GroupMessageEvent event, List<String> params) {
-        if (params.isEmpty())
-            throw new NullBotException("[基质规划] ❌未指定武器");
-        String weapon = params.getFirst();
-        // String weapon = String.join(" ", params.subList(0, params.size()));
-
-        String base64;
-        try {
-            base64 = webScreenCapturer.capture(
-                    "https://end.canmoe.com/", 1536, 5120,
-                    List.of("//section[contains(@class,'panel')][.//h2[contains(text(),'方案推荐列表')]]"),
-                    List.of(".ghost-button"),
-                    List.of(
-                            "#app > div > div > div.notice-footer > div.about-actions > button",
-                            String.format("//span[@class='weapon-title-text' and text()='%s']", weapon),
-                            "//button[contains(.,'收起其他方案')]"
-                    )
-            );
-        } catch (Exception e) {
-            throw new NullBotException("[基质规划] ❌查询失败: " + e.getMessage());
-        }
-
+    public void execute(Bot bot, GroupMessageEvent event, CommandArgs params) {
+        String weapon = params.nextFullString();
+        String base64 = webScreenCapturer.capture(
+                "https://end.canmoe.com/", 1536, 5120,
+                List.of("//section[contains(@class,'panel')][.//h2[contains(text(),'方案推荐列表')]]"),
+                List.of(".ghost-button"),
+                List.of(
+                        "#app > div > div > div.notice-footer > div.about-actions > button",
+                        String.format("//span[@class='weapon-title-text' and text()='%s']", weapon),
+                        "//button[contains(.,'收起其他方案')]"
+                )
+        );
         String response = MsgUtils.builder().img("base64://" + base64).build();
         bot.sendGroupMsg(event.getGroupId(), response, false);
-        log.info("├─[EssencePlan] 已查询 - {}", weapon);
+        log.info("☑ [EssencePlan] 已查询 - {}", weapon);
     }
 
     @Override
