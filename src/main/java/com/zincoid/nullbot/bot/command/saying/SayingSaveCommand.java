47d16c5668669397e7dcaa52c50de8386ceb640b
Zincoid <2660181154@qq.com>
2026-05-26 23:14:30 +0800

    NullBot 后端SpringBoot工程

--- src/main/java/com/zincoid/nullbot/bot/command/saying/SayingSaveCommand.java
+++ src/main/java/com/zincoid/nullbot/bot/command/saying/SayingSaveCommand.java
@@ -5,6 +5,7 @@ import com.mikuac.shiro.dto.action.response.MsgResp;
 import com.mikuac.shiro.dto.event.message.GroupMessageEvent;
 import com.mikuac.shiro.enums.MsgTypeEnum;
 import com.mikuac.shiro.model.ArrayMsg;
+import com.zincoid.nullbot.bot.command.CommandArgs;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import com.zincoid.nullbot.core.annotation.CommandMapping;
@@ -14,33 +15,26 @@ import com.zincoid.nullbot.core.util.MsgParseUtil;
 import com.zincoid.nullbot.core.service.SayingService;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-
+@Slf4j
 @CommandMapping({"SayingSave", "保存语录"})
 @Component
 @RequiredArgsConstructor
-@Slf4j
 public class SayingSaveCommand implements Command {
 
     private final SayingService sayingService;
 
     @Override
-    public void execute(Bot bot, GroupMessageEvent event, List<String> params) {
+    public void execute(Bot bot, GroupMessageEvent event, CommandArgs params) {
         ArrayMsg reply = event.getArrayMsg().getFirst();
         if (reply.getType() != MsgTypeEnum.reply)
-            throw new NullBotException("[保存语录] ❌需引用文本");
+            throw new NullBotException("需引用文本");
         MsgResp replyMsg = bot.getMsg(reply.getData().get("id").asInt()).getData();
         long userId = Long.parseLong(replyMsg.getSender().getUserId());
         String userName = replyMsg.getSender().getNickname();
-        String text;
-        try {
-            text = MsgParseUtil.formatSaying(bot, replyMsg.getRawMessage());
-        } catch (Exception e) {
-            throw new NullBotException("[保存语录] ❌" + e.getMessage());
-        }
+        String text = MsgParseUtil.formatSaying(bot, replyMsg.getRawMessage());
         int inserted = sayingService.add(userId, userName, text);
-        bot.sendGroupMsg(event.getGroupId(), inserted == 1 ? "\uD83D\uDCBE 已记录！" : "[保存语录] ❌出错", false);
-        log.info("├─[SayingSave] 语录保存 - {} -> {}", text, inserted == 1 ? "已记录" : "出错");
+        bot.sendGroupMsg(event.getGroupId(), inserted == 1 ? "\uD83D\uDCBE 已记录！" : "出错", false);
+        log.info("☑ [SayingSave] 语录保存 - {} -> {}", text, inserted == 1 ? "已记录" : "出错");
     }
 
     @Override
