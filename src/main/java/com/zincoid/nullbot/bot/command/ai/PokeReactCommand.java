65dda4963148e64ba90f2cdf8ddc42e40868bd79
Zincoid <2660181154@qq.com>
2026-05-26 19:04:13 +0800

    NullBot 后端SpringBoot工程

--- src/main/java/com/zincoid/nullbot/bot/command/ai/PokeReactCommand.java
+++ src/main/java/com/zincoid/nullbot/bot/command/ai/PokeReactCommand.java
@@ -2,7 +2,7 @@ package com.zincoid.nullbot.bot.command.ai;
 
 import com.mikuac.shiro.core.Bot;
 import com.mikuac.shiro.dto.event.notice.PokeNoticeEvent;
-import com.zincoid.nullbot.bot.exception.NullBotException;
+import com.zincoid.nullbot.bot.command.CommandArgs;
 import com.zincoid.nullbot.core.component.ai.chat.client.QQAiClient;
 import com.zincoid.nullbot.core.component.ai.chat.message.QQMessage;
 import lombok.extern.slf4j.Slf4j;
@@ -11,12 +11,11 @@ import com.zincoid.nullbot.bot.command.Command;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
 import java.util.Objects;
 
+@Slf4j
 @CommandMapping({"PokeReact"})
 @Component
-@Slf4j
 public class PokeReactCommand implements Command {
 
     private final QQAiClient qqAiClient;
@@ -26,25 +25,16 @@ public class PokeReactCommand implements Command {
     }
 
     @Override
-    public void execute(Bot bot, PokeNoticeEvent event, List<String> params) {
-        if (!Objects.equals(event.getTargetId(), event.getSelfId())) return;  // 仅检测戳Bot
+    public void execute(Bot bot, PokeNoticeEvent event, CommandArgs params) {
+        if (!Objects.equals(event.getTargetId(), event.getSelfId())) return;  // 仅检测戳 Bot 自身
         Long groupId = event.getGroupId();
         Long userId = event.getUserId();
         String userName = bot.getStrangerInfo(userId, true).getData().getNickname();
         String response;
-        try {
-            if (groupId != null) {
-                QQMessage message = QQMessage.user("揉了你一下").with(groupId, userId, userName);
-                response = qqAiClient.chat(message);
-            } else {
-                QQMessage message = QQMessage.user("揉了你一下").with(userId, userName);
-                response = qqAiClient.chat(message);
-            }
-        } catch (Exception e) {
-            throw new NullBotException("[AI] ❌出错: " + e.getMessage());
-        }
-        log.info("├─[PokeReact] 已回复{}戳戳: {}", groupId != null ? "群聊" : "私聊", response);
+        QQMessage message;
+        if (groupId != null) message = QQMessage.user("揉了你一下").with(groupId, userId, userName);
+        else message = QQMessage.user("揉了你一下").with(userId, userName);
+        response = qqAiClient.chat(message);
+        log.info("☑ [PokeReact] {}戳戳已回复: {}", groupId != null ? "群聊" : "私聊", response);
     }
-
-    // 特殊命令 无帮助
 }
