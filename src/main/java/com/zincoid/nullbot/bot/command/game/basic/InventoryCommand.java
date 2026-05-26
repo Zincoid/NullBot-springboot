39c99ffe5aa8eab71b72962128b5bbc084d3c07e
Zincoid <2660181154@qq.com>
2026-05-26 20:48:54 +0800

    NullBot 后端SpringBoot工程

--- src/main/java/com/zincoid/nullbot/bot/command/game/basic/InventoryCommand.java
+++ src/main/java/com/zincoid/nullbot/bot/command/game/basic/InventoryCommand.java
@@ -2,6 +2,7 @@ package com.zincoid.nullbot.bot.command.game.basic;
 
 import com.mikuac.shiro.core.Bot;
 import com.mikuac.shiro.dto.event.message.GroupMessageEvent;
+import com.zincoid.nullbot.bot.command.CommandArgs;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import com.zincoid.nullbot.core.annotation.CommandMapping;
@@ -9,32 +10,22 @@ import com.zincoid.nullbot.bot.command.Command;
 import com.zincoid.nullbot.core.model.data.DataPage;
 import com.zincoid.nullbot.core.model.data.po.UserPO;
 import com.zincoid.nullbot.core.model.data.vo.InventoryVO;
-import com.zincoid.nullbot.bot.exception.NullBotException;
 import com.zincoid.nullbot.core.service.InventoryService;
 import com.zincoid.nullbot.core.service.UserService;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-
-
+@Slf4j
 @CommandMapping({"Inventory", "查看库存", "库存"})
 @Component
 @RequiredArgsConstructor
-@Slf4j
 public class InventoryCommand implements Command {
 
     private final InventoryService inventoryService;
     private final UserService userService;
 
     @Override
-    public void execute(Bot bot, GroupMessageEvent event, List<String> params) {
-        int p = 1;
-        if(!params.isEmpty())
-            try {
-                p = Integer.parseInt(params.getFirst());
-            } catch (NumberFormatException e) {
-                throw new NullBotException("[库存] ❌页码格式错误");
-            }
+    public void execute(Bot bot, GroupMessageEvent event, CommandArgs params) {
+        int p = params.nextIntOptional(1);
         Long userId = event.getUserId();
         String userName = event.getSender().getNickname();
         DataPage<InventoryVO> inventoryVOPage = inventoryService.getVOPage(userId, p, 10);
@@ -53,7 +44,7 @@ public class InventoryCommand implements Command {
         }
         sb.append("[第").append(inventoryVOPage.getCurrent()).append("页").append(" / 共").append(inventoryVOPage.getPages()).append("页 (每页").append(inventoryVOPage.getSize()).append("条)]");
         bot.sendGroupMsg(event.getGroupId(), sb.toString(), false);
-        log.info("├─[Inventory] 已获取库存 - {}({})", userName, userId);
+        log.info("☑ [Inventory] 库存已获取 - UserId: {}", userId);
     }
 
     @Override
