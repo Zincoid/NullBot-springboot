9bba2321b60ca80e44c466da706de00d06df54fb
Zincoid <2660181154@qq.com>
2026-05-26 17:38:24 +0800

    NullBot 后端SpringBoot工程

--- src/main/java/com/zincoid/nullbot/bot/command/Command.java
+++ src/main/java/com/zincoid/nullbot/bot/command/Command.java
@@ -6,14 +6,14 @@ import com.mikuac.shiro.dto.event.message.PrivateMessageEvent;
 import com.mikuac.shiro.dto.event.notice.GroupMsgDeleteNoticeEvent;
 import com.mikuac.shiro.dto.event.notice.PokeNoticeEvent;
 import com.zincoid.nullbot.bot.exception.NullBotException;
-import com.zincoid.nullbot.bot.exception.NullBotLogException;
 
 import java.util.List;
 
 public interface Command {
-    default void execute(Bot bot, GroupMessageEvent event, List<String> params) throws Exception { throw new NullBotLogException("暂无群消息事件响应方式"); }
-    default void execute(Bot bot, PokeNoticeEvent event, List<String> params) throws Exception { throw new NullBotLogException("暂无群戳戳事件响应方式"); }
-    default void execute(Bot bot, GroupMsgDeleteNoticeEvent event, List<String> params) throws Exception { throw new NullBotLogException("暂无群撤回事件响应方式"); }
+
+    default void execute(Bot bot, GroupMessageEvent event, List<String> params) throws Exception { throw new NullBotException("暂无群消息事件响应方式"); }
+    default void execute(Bot bot, PokeNoticeEvent event, List<String> params) throws Exception { throw new NullBotException("暂无群戳戳事件响应方式"); }
+    default void execute(Bot bot, GroupMsgDeleteNoticeEvent event, List<String> params) throws Exception { throw new NullBotException("暂无群撤回事件响应方式"); }
     default void execute(Bot bot, PrivateMessageEvent event, List<String> params) throws Exception { throw new NullBotException("暂无私信事件响应方式"); }
 
     default String getHelp() { return "暂无帮助文档"; }
