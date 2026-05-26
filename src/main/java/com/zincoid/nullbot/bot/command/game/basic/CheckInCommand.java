38dc473893c2042b4e6bbef6f154da937517ec76
Zincoid <2660181154@qq.com>
2026-05-26 20:47:27 +0800

    NullBot 后端SpringBoot工程

--- src/main/java/com/zincoid/nullbot/bot/command/game/basic/CheckInCommand.java
+++ src/main/java/com/zincoid/nullbot/bot/command/game/basic/CheckInCommand.java
@@ -2,6 +2,7 @@ package com.zincoid.nullbot.bot.command.game.basic;
 
 import com.mikuac.shiro.core.Bot;
 import com.mikuac.shiro.dto.event.message.GroupMessageEvent;
+import com.zincoid.nullbot.bot.command.CommandArgs;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import com.zincoid.nullbot.core.annotation.CommandMapping;
@@ -12,21 +13,20 @@ import org.springframework.stereotype.Component;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
-import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+@Slf4j
 @CommandMapping({"CheckIn", "签到"})
 @Component
 @RequiredArgsConstructor
-@Slf4j
 public class CheckInCommand implements Command {
 
     private final Map<Long, LocalDateTime> checkInExpireMap = new ConcurrentHashMap<>();
     private final UserService userService;
 
     @Override
-    public void execute(Bot bot, GroupMessageEvent event, List<String> params) {
+    public void execute(Bot bot, GroupMessageEvent event, CommandArgs params) {
         Long userId = event.getUserId();
         String userName = event.getSender().getNickname();
         LocalDateTime expireTime = checkInExpireMap.get(userId);
@@ -34,10 +34,10 @@ public class CheckInCommand implements Command {
             userService.increaseDrawTimes(userId, 25);
             checkInExpireMap.put(userId, LocalDate.now().atTime(LocalTime.MAX));
             bot.sendGroupMsg(event.getGroupId(), "✨" + userName + " 签到成功！获得: \n- 抽奖次数 x 25", false);
-            log.info("├─[CheckIn] 签到成功 - 用户 {}", userId);
+            log.info("☑ [CheckIn] 用户签到成功 - UserId: {}", userId);
         } else {
             bot.sendGroupMsg(event.getGroupId(), userName + " 今日已签过到！", false);
-            log.info("├─[CheckIn] 今日已签过到 - 用户 {}", userId);
+            log.info("☑ [CheckIn] 今日已签过到 - UserId: {}", userId);
         }
     }
 
