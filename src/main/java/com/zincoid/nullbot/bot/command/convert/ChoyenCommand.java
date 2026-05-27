df48d9c1b8c6a0cb774cfa2e72466632f194da38
Zincoid <2660181154@qq.com>
2026-05-28 01:01:11 +0800

    NullBot 后端SpringBoot工程

--- src/main/java/com/zincoid/nullbot/bot/command/convert/ChoyenCommand.java
+++ src/main/java/com/zincoid/nullbot/bot/command/convert/ChoyenCommand.java
@@ -4,17 +4,12 @@ import com.mikuac.shiro.common.utils.MsgUtils;
 import com.mikuac.shiro.core.Bot;
 import com.mikuac.shiro.dto.event.message.GroupMessageEvent;
 import com.zincoid.nullbot.bot.command.CommandArgs;
-import com.zincoid.nullbot.core.util.HtmlUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import com.zincoid.nullbot.core.annotation.CommandMapping;
 import com.zincoid.nullbot.bot.command.Command;
 import com.zincoid.nullbot.core.component.render.HtmlRenderer;
-import com.zincoid.nullbot.core.component.resource.ResourceLoader;
 import org.springframework.stereotype.Component;
-
-import java.nio.file.Path;
-import java.util.*;
 
 @Slf4j
 @CommandMapping({"Choyen", "5000兆"})
@@ -22,25 +17,16 @@ import java.util.*;
 @RequiredArgsConstructor
 public class ChoyenCommand implements Command {
 
-    private final ResourceLoader resourceLoader;
     private final HtmlRenderer htmlRenderer;
 
     @Override
     public void execute(Bot bot, GroupMessageEvent event, CommandArgs args) throws Exception {
         Long groupId = event.getGroupId();
 
-        Path htmlPath = resourceLoader.getCache("static/html/5000choyen.html");
-        String html = HtmlUtil.loadTemplate(htmlPath.toString());
-
-        Map<String, String> variables = Map.of(
-                "topText", args.nextString(),
-                "bottomText", args.nextString()
-        );
-
-        String base64 = htmlRenderer.renderElement(
-                HtmlUtil.replaceVariables(html, variables),
-                "#templateContainer"
-        );
+        String base64 = htmlRenderer.load("static/html/5000choyen.html")
+                .set("topText", args.nextString())
+                .set("bottomText", args.nextString())
+                .renderElement("#templateContainer");
 
         String response = MsgUtils.builder().img("base64://" + base64).build();
         bot.sendGroupMsg(groupId, response, false);
