92f365006e691ff15931c0545acccf48044663ba
Zincoid <2660181154@qq.com>
2026-08-11 01:10:27 +0800

    feat(game): 更新Wife命令功能支持参数选项

--- src/main/java/com/zincoid/nullbot/bot/command/game/indie/WifeCmd.java
+++ src/main/java/com/zincoid/nullbot/bot/command/game/indie/WifeCmd.java
@@ -8,8 +8,10 @@ import com.mikuac.shiro.dto.event.message.GroupMessageEvent;
 import com.zincoid.nullbot.bot.command.Cmd;
 import com.zincoid.nullbot.bot.command.CmdArgs;
 import com.zincoid.nullbot.bot.exception.BotInfoException;
+import com.zincoid.nullbot.bot.exception.BotWarnException;
 import com.zincoid.nullbot.core.enums.Emoji;
 import com.zincoid.nullbot.core.module.resource.builder.ResourceUrlBuilder;
+import com.zincoid.nullbot.core.service.base.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import com.zincoid.nullbot.core.annotation.CmdMapping;
@@ -38,20 +40,32 @@ public class WifeCmd implements Cmd {
     private final Map<Long, FilePO> acgWifeMap = new ConcurrentHashMap<>();
     private final Map<Long, LocalDateTime> acgExpireMap = new ConcurrentHashMap<>();
 
+    private final UserService userService;
     private final StorageProperties storageProperties;
     private final FileService fileService;
     private final ResourceUrlBuilder resourceUrlBuilder;
 
     @Override
     public void run(Bot bot, GroupMessageEvent event, CmdArgs args) {
-        if (args.hasNext()) {
-            animeWife(bot, event, args);
-        } else {
-            memberWife(bot, event);
+        if (args.hasOpt("member", "m")) {
+            int access = userService.getAccess(event.getUserId());
+            if (access < 1) throw new BotWarnException("你不能用。");
+            Long wifeId = args.optLong("member", "m");
+            setMemberWife(bot, event, wifeId);
+            return;
         }
+        if (args.hasOpt("anime", "a")) {
+            int access = userService.getAccess(event.getUserId());
+            if (access < 1) throw new BotWarnException("你不能用。");
+            String keyword = args.getOpt("anime", "a");
+            setAnimeWife(bot, event, args.next(), keyword);
+            return;
+        }
+        if (!args.hasNext()) { randomMemberWife(bot, event); }
+        else { randomAnimeWife(bot, event, args); }
     }
 
-    private void memberWife(Bot bot, GroupMessageEvent event) {
+    private void randomMemberWife(Bot bot, GroupMessageEvent event) {
         Long userId = event.getUserId();
         LocalDateTime expireTime = memberExpireMap.get(userId);
         if (expireTime != null && expireTime.isAfter(LocalDateTime.now())) {
@@ -76,8 +90,12 @@ public class WifeCmd implements Cmd {
             int randomIndex = ThreadLocalRandom.current().nextInt(members.size());
             wife = members.get(randomIndex);
         } while (Objects.equals(wife.getUserId(), event.getUserId()));
-        Long wifeId = wife.getUserId();
-        String wifeName = wife.getNickname();
+        setMemberWife(bot, event, wife.getUserId());
+    }
+
+    private void setMemberWife(Bot bot, GroupMessageEvent event, Long wifeId) {
+        Long userId = event.getUserId();
+        String wifeName = bot.getStrangerInfo(wifeId, true).getData().getNickname();
         String avatarUrl = ShiroUtils.getUserAvatar(wifeId, 5);
         String response = MsgUtils.builder()
                 .at(userId)
@@ -92,7 +110,7 @@ public class WifeCmd implements Cmd {
         log.info("☑ [Wife] 今日群友老婆 - {} -> {}", userId, wifeId);
     }
 
-    private void animeWife(Bot bot, GroupMessageEvent event, CmdArgs args) {
+    private void randomAnimeWife(Bot bot, GroupMessageEvent event, CmdArgs args) {
         Long userId = event.getUserId();
         LocalDateTime expireTime = acgExpireMap.get(userId);
         if (expireTime != null && expireTime.isAfter(LocalDateTime.now())) {
@@ -110,9 +128,13 @@ public class WifeCmd implements Cmd {
             log.info("☑ [Wife] 今日已选过二次元老婆 - {} -> {}", userId, wifeName);
             return;
         }
-        String category = args.next();
+        setAnimeWife(bot, event, args.next(), null);
+    }
+
+    private void setAnimeWife(Bot bot, GroupMessageEvent event, String category, String keyword) {
+        Long userId = event.getUserId();
         String acgPath = storageProperties.getImagePath() + "/acg/" + category;
-        List<FilePO> wives = fileService.list(acgPath);
+        List<FilePO> wives = fileService.search(keyword, acgPath);
         if (wives.isEmpty())
             throw new BotInfoException(Emoji.INFO, "暂无角色");
         FilePO wife = wives.get(ThreadLocalRandom.current().nextInt(wives.size()));
@@ -136,9 +158,15 @@ public class WifeCmd implements Cmd {
                 ◉ Wife 命令
                 功能: 今日老婆
                 限权: %d 级
-                格式: Wife [可选: 人物来源]
+                格式: Wife [可选: 人物来源] [选项]
+                
+                选项:
+                -m,--member=[用户ID]  指定群友老婆
+                -a,--anime=[关键字]  指定二次元老婆
+                
                 注意:
                 - 无参数时选择群友老婆, 带参数时选二次元老婆
+                - 指定功能需限权I及以上
                 - 以上两种选择每日均可抽一次
                 - 人物来源可通过图片目录命令查看acg下子目录名获得
                 别名: 今日老婆""", getAccess()
