3cc85b01edddeec280350a5c2a60e132de9ed954
Zincoid <2660181154@qq.com>
2026-09-10 23:10:50 +0800

    refactor(command): 更新指令接口默认异常消息
    - 修改群聊事件异常提示为"指令不支持群聊事件"
    - 修改戳戳事件异常提示为"指令不支持戳一戳事件"
    - 修改群撤回事件异常提示为"指令不支持群撤回事件"
    - 修改私聊事件异常提示为"指令不支持私聊事件"

--- src/main/java/com/zincoid/nullbot/bot/command/Cmd.java
+++ src/main/java/com/zincoid/nullbot/bot/command/Cmd.java
@@ -9,10 +9,10 @@ import com.zincoid.nullbot.bot.exception.BotWarnException;
 
 public interface Cmd {
 
-    default void run(Bot bot, GroupMessageEvent event, CmdArgs args) throws Exception { throw new BotWarnException("暂无群聊事件响应方式"); }
-    default void run(Bot bot, PokeNoticeEvent event, CmdArgs args) throws Exception { throw new BotWarnException("暂无戳戳事件响应方式"); }
-    default void run(Bot bot, GroupMsgDeleteNoticeEvent event, CmdArgs args) throws Exception { throw new BotWarnException("暂无群撤回事件响应方式"); }
-    default void run(Bot bot, PrivateMessageEvent event, CmdArgs args) throws Exception { throw new BotWarnException("暂无私聊事件响应方式"); }
+    default void run(Bot bot, GroupMessageEvent event, CmdArgs args) throws Exception { throw new BotWarnException("指令不支持群聊事件"); }
+    default void run(Bot bot, PokeNoticeEvent event, CmdArgs args) throws Exception { throw new BotWarnException("指令不支持戳一戳事件"); }
+    default void run(Bot bot, GroupMsgDeleteNoticeEvent event, CmdArgs args) throws Exception { throw new BotWarnException("指令不支持群撤回事件"); }
+    default void run(Bot bot, PrivateMessageEvent event, CmdArgs args) throws Exception { throw new BotWarnException("指令不支持私聊事件"); }
 
     default String getHelp() { return "暂无帮助文档"; }
     default String getHelpForAI() { return getHelp(); }
