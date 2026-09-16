9a4f910ebe8a75d1052ee25fba79d4e6132de02e
Zincoid <2660181154@qq.com>
2026-09-16 19:40:38 +0800

    refactor(core): 优化设置转换器映射配置
    - 添加 MapStruct Mapping 注解以忽略 ID 字段映射
    - 防止 DTO 转 PO 时意外覆盖自动生成的 ID 值
    - 提升数据转换的安全性和一致性

--- src/main/java/com/zincoid/nullbot/core/converter/SettingConverter.java
+++ src/main/java/com/zincoid/nullbot/core/converter/SettingConverter.java
@@ -3,6 +3,7 @@ package com.zincoid.nullbot.core.converter;
 import com.zincoid.nullbot.core.model.data.dto.SettingDTO;
 import com.zincoid.nullbot.core.model.data.po.SettingPO;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
@@ -10,5 +11,6 @@ public interface SettingConverter {
 
     SettingConverter INSTANCE = Mappers.getMapper(SettingConverter.class);
 
+    @Mapping(target = "id", ignore = true)
     SettingPO toPO(SettingDTO dto);
 }
